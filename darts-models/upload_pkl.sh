da5adbd1415163774a86cdb3215a6b47635c4a2e
Ilshat Saifullin <I.S.Saifullin@tudelft.nl>
2023-04-03 13:25:36 +0200

    upload pkl bash script fixes

--- darts-models/upload_pkl.sh
+++ darts-models/upload_pkl.sh
@@ -1,4 +1,5 @@
 tar -czf "$CI_COMMIT_REF_NAME".tar.gz ./*/*.pkl
-smbclient -U $2%$3 //$1/darts-private-artifacts -c "mkdir $CI_COMMIT_REF_NAME"
-smbclient -U $2%$3 //$1/darts-private-artifacts/open-darts/pkl -c "put "$CI_COMMIT_REF_NAME".tar.gz -D="$CI_COMMIT_REF_NAME"
+smbclient -U $2%$3 //$1/opendarts-private-artifacts -c "prompt OFF;mkdir pkl"
+smbclient -U $2%$3 //$1/opendarts-private-artifacts -c "put $CI_COMMIT_REF_NAME.tar.gz" -D=pkl
+
 
