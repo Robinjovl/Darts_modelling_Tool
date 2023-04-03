3d80b0540eb1414f3a4b648d60fa60e273f51a23
Ilshat Saifullin <I.S.Saifullin@tudelft.nl>
2023-04-03 14:09:44 +0200

    del archive after upload; 4-th arg

--- darts-models/upload_pkl.sh
+++ darts-models/upload_pkl.sh
@@ -1,5 +1,5 @@
-tar -czf "$CI_COMMIT_REF_NAME".tar.gz ./*/*.pkl
+commit=$4
+tar -czf $commit.tar.gz ./*/*.pkl
 smbclient -U $2%$3 //$1/opendarts-private-artifacts -c "prompt OFF;mkdir pkl"
-smbclient -U $2%$3 //$1/opendarts-private-artifacts -c "put $CI_COMMIT_REF_NAME.tar.gz" -D=pkl
-
-
+smbclient -U $2%$3 //$1/opendarts-private-artifacts -c "put $commit.tar.gz" -D=pkl
+rm $commit.tar.gz
