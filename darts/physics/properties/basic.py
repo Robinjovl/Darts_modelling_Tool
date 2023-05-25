f1e0daabf4c6faee0a3362e17d18f38f5b727c6a
Michiel Wapperom <m.b.wapperom@tudelft.nl>
2023-05-25 16:47:19 +0200

    Add darts/physics with properties/ folder, each property has own file with predefined correlations

---
+++ darts/physics/properties/basic.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+
+class ConstFunc:
+    def __init__(self, value):
+        super().__init__()
+        self.value = value
+
+    def evaluate(self, dummy1=0, dummy2=0, dummy3=0, dummy4=0):
+        return self.value
+
+
+class PhaseRelPerm:
+    def __init__(self, phase, swc=0, sgr=0):
+        self.phase = phase
+
+        self.Swc = swc
+        self.Sgr = sgr
+        if phase == "oil":
+            self.kre = 1
+            self.sr = self.Swc
+            self.sr1 = self.Sgr
+            self.n = 2
+        elif phase == 'gas':
+            self.kre = 1
+            self.sr = self.Sgr
+            self.sr1 = self.Swc
+            self.n = 2
+        else:  # water
+            self.kre = 1
+            self.sr = 0
+            self.sr1 = 0
+            self.n = 2
+
+    def evaluate(self, sat):
+        if sat >= 1 - self.sr1:
+            kr = self.kre
+        elif sat <= self.sr:
+            kr = 0
+        else:
+            # general Brook-Corey
+            kr = self.kre * ((sat - self.sr) / (1 - self.Sgr - self.Swc)) ** self.n
+
+        return kr
+
+
+class CapillaryPressure:
+    def __init__(self, p_entry=0, swc=0, labda=2):
+        self.swc = swc
+        self.p_entry = p_entry
+        self.labda = labda
+        self.eps = 1e-3
+
+    def evaluate(self, sat):
+        '''
+        default evaluator of capillary pressure Pc based on pow
+        :param sat: saturation
+        :return: Pc
+        '''
+        Se = (sat - self.swc)/(1 - self.swc)
+        if Se < self.eps:
+            Se = self.eps
+        pc = self.p_entry * Se ** (-1/self.labda)
+
+        Pc = np.array([0, pc], dtype=object)
+
+        return Pc
+
+
+class RockCompactionEvaluator:
+    def __init__(self, pref=1, compres=1.45e-5):
+        self.Pref = pref
+        self.compres = compres
+
+    def evaluate(self, pressure):
+        return 1.0 + self.compres * (pressure - self.Pref)
+
+
+class RockEnergyEvaluator:
+    def __init__(self, c_vr=3710., T_ref=273.15):
+        self.c_vr = c_vr  # 1400 J/kg.K * 2650 kg/m3 -> kJ/m3
+        self.T_ref = T_ref
+
+    def evaluate(self, temperature):
+        return self.c_vr * (temperature - self.T_ref)  # kJ/m3
---
+++ darts/physics/properties/kinetics.py
@@ -0,0 +1,120 @@
+import abc
+import warnings
+import numpy as np
+from .flash import SolidFlash
+
+
+class Kinetics:
+    def __init__(self, stoich: list):
+        self.stoich = stoich
+
+    @abc.abstractmethod
+    def evaluate(self, pressure, temperature, x, sat):
+        pass
+
+    def evaluate_enthalpy(self, pressure, temperature, x, sat):
+        return 0.
+
+
+class KineticsBasic(Kinetics):
+    """
+    Law of Mass Action for kinetic reaction
+    For reaction aA + bB <-> cC: rate = c * (1 - Q/K) with Q = [C]^c / [A]^a [B]^b
+    """
+    def __init__(self, stoich: list, prod: list, equi_prod: float, kin_rate_cte: float, combined_ions=True):
+        super().__init__(stoich)
+
+        self.prod = prod
+        self.equi_prod = equi_prod
+        self.kin_rate_cte = kin_rate_cte
+        self.combined_ions = combined_ions
+        self.ne = len(stoich)
+
+    def evaluate(self, pressure, temperature, x, sat_sol):
+        # For reaction aA + bB <-> cC
+        # Calculate activity product Q = [C]^c / [A]^a [B]^b
+        prod = 1.
+        for i in range(self.ne):
+            prod *= x[i] ** self.stoich[i]
+
+        # Calculate rate = c * As * (1-Q/K)
+        rate = self.kin_rate_cte * sat_sol * (1. - prod / self.equi_prod)
+
+        return [stoich * rate for stoich in self.stoich]
+
+
+class HydrateKinetics(Kinetics):
+    def __init__(self, components: list, Mw, flash: SolidFlash, hydrate_eos, fluid_eos: list, stoich: list = None,
+                 perm=300., poro=0.2, k=3.11e12, enthalpy: bool = False):
+        super().__init__(stoich)
+
+        self.flash = flash
+        self.hydrate_eos = hydrate_eos
+        self.fluid_eos = fluid_eos
+
+        self.water_idx = components.index("H2O")
+        self.guest_idx = 0 if self.water_idx == 1 else 1
+        self.hydrate_idx = components.index("H")
+
+        self.stoich = stoich
+        if stoich is not None:
+            self.nH = stoich[self.water_idx]/stoich[self.guest_idx]
+        else:
+            self.nH = None
+        self.Mw = Mw
+
+        self.K = k  # reaction constant [kmol/(m^2 bar day)]
+        self.perm = perm * 1E-15  # mD to m2
+        self.poro = poro
+
+        self.enthalpy = enthalpy
+
+    def evaluate(self, pressure, temperature, x, sat):
+        # Calculate fugacity difference between water in fluid phases and water in hydrate phase
+        if x[0, 0] != 0.:
+            f0 = self.flash.flash.fugacity(pressure, temperature, x[0, :], self.fluid_eos[0])
+        else:
+            f0 = self.flash.flash.fugacity(pressure, temperature, x[1, :], self.fluid_eos[1])
+
+        self.hydrate_eos.component_parameters(pressure, temperature)
+        fwH = self.hydrate_eos.fwH(f0)
+
+        df = fwH - f0[self.water_idx]  # if df < 0 formation, if df > 0 dissociation
+        xH = self.hydrate_eos.xH()
+
+        # Reaction rate following Yin (2018)
+        # surface area
+        F_A = 1
+        r_p = np.sqrt(45 * self.perm * (1 - self.poro) ** 2 / (self.poro ** 3))
+        A_s = 0.879 * F_A * (1 - self.poro) / r_p * sat ** (2 / 3)  # hydrate surface area [m2]
+
+        # Thermodynamic parameters
+        dE = -81E3  # activation energy [J/mol]
+        R = 8.3145  # gas constant [J/(K.mol)]
+
+        # K is reaction cons, A_s hydrate surface area, dE activation energy, driving force is fugacity difference
+        self.rate = self.K * A_s * np.exp(dE / (R * temperature)) * df
+
+        return [stoich * self.rate for stoich in self.stoich]
+
+    def evaluate_enthalpy(self, pressure, temperature, x, sat):
+        if self.enthalpy:
+            # Enthalpy change with dissociation (-ive, rate of hydrate component +ive)
+            Cf = 33.72995  # J/kg cal/gmol
+            if temperature - 273.15 > 0:
+                (C1, C2) = (13521, -4.02)
+            else:
+                (C1, C2) = (6534, -11.97)
+            en = Cf * (C1 + C2 / temperature) * 1e-3  # kJ/kg
+
+            # Calculate molar weight of hydrate component
+            if self.nH is None:
+                mH = np.sum(x * self.Mw)
+            else:
+                mH = self.Mw[self.water_idx] * self.nH + self.Mw[self.guest_idx]
+
+            H_diss = -en * mH  # kJ/kg * kg/kmol -> kJ/kmol
+
+            return self.rate * H_diss  # rate * enth/mol
+        else:
+            return 0.
