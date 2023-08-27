d9f3a9fcace95a8744b36858c49c1bbb4a3de810
Ilshat Saifullin <I.S.Saifullin@tudelft.nl>
2023-08-27 20:50:44 +0200

    no co2foam run

--- models/run_test_suite2.py
+++ models/run_test_suite2.py
@@ -12,7 +12,7 @@ accepted_dirs = ['2ph_comp', '2ph_comp_solid', '2ph_do', '2ph_do_thermal',
                  '3ph_comp_w', '3ph_do', '3ph_bo',
                  'Uniform_Brugge',
                  'Chem_benchmark_new',
-                 'CO2_foam_CCS',
+                 #'CO2_foam_CCS',
                  'GeoRising',
                  'CoaxWell'
                  ]
