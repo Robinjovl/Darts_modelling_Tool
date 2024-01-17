6d92137523daedf09997e9528d97e77df24bee9c
Ilshat Saifullin <I.S.Saifullin@tudelft.nl>
2024-01-17 21:49:38 +0100

    added pkl files for terzaghi_two_layers_mech_discretizer

--- models/1ph_1comp_poroelastic_analytics/main.py
+++ models/1ph_1comp_poroelastic_analytics/main.py
@@ -577,8 +577,8 @@ def get_solution_slice(m, discr_name, mesh, sol_data):
 # run_and_plot(case='bai', discretizer='mech_discretizer', mesh='hex')
 
 
-test_all = False
-#test_all = True
+#test_all = False
+test_all = True
 cases_list = ['terzaghi', 'mandel', 'terzaghi_two_layers', 'bai']
 if test_all:
     for case in cases_list:
