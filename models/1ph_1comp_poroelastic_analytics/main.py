b954996228151520e7cb18e00e837f0211a41e8c
Ilshat Saifullin <I.S.Saifullin@tudelft.nl>
2024-02-06 22:45:19 +0100

    get back some uncommented code

--- models/1ph_1comp_poroelastic_analytics/main.py
+++ models/1ph_1comp_poroelastic_analytics/main.py
@@ -554,13 +554,13 @@ def get_solution_slice(m, discr_name, mesh, sol_data):
     return sol_data_slice
 
 # Rectangular grid, comparison to analytics
-run_and_plot(case='terzaghi', discretizer='mech_discretizer', mesh='rect')
-run_and_plot(case='terzaghi', discretizer='pm_discretizer', mesh='rect')
-run_and_plot(case='mandel', discretizer='mech_discretizer', mesh='rect')
-run_and_plot(case='mandel', discretizer='pm_discretizer', mesh='rect')
-run_and_plot(case='terzaghi_two_layers', discretizer='pm_discretizer', mesh='rect')
-run_and_plot(case='terzaghi_two_layers', discretizer='mech_discretizer', mesh='rect')
-run_and_plot(case='bai', discretizer='mech_discretizer', mesh='rect')
+#run_and_plot(case='terzaghi', discretizer='mech_discretizer', mesh='rect')
+#run_and_plot(case='terzaghi', discretizer='pm_discretizer', mesh='rect')
+#run_and_plot(case='mandel', discretizer='mech_discretizer', mesh='rect')
+#run_and_plot(case='mandel', discretizer='pm_discretizer', mesh='rect')
+#run_and_plot(case='terzaghi_two_layers', discretizer='pm_discretizer', mesh='rect')
+#run_and_plot(case='terzaghi_two_layers', discretizer='mech_discretizer', mesh='rect')
+#run_and_plot(case='bai', discretizer='mech_discretizer', mesh='rect')
 
 # Wedge (triangular) grid
 #run(case='terzaghi', discretizer='mech_discretizer', mesh='wedge')
