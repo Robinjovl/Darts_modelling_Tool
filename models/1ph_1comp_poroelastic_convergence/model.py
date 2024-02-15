8adb31eee6a2ef01653b8c30290f465ca8089f8f
Ilshat Saifullin <I.S.Saifullin@tudelft.nl>
2024-02-15 11:01:11 +0100

    removed params settings for convergence test as it is already in a base model

--- models/1ph_1comp_poroelastic_convergence/model.py
+++ models/1ph_1comp_poroelastic_convergence/model.py
@@ -13,25 +13,6 @@ class Model(OnePhaseThermoPoroElasticModel):
         self.heat_cond_mult = heat_cond_mult
         super().__init__(n_points=n_points, discretizer=discretizer)
 
-        self.params.tolerance_newton = 1e-6 # Tolerance of newton residual norm ||residual||<tol_newt
-        self.params.newton_type = sim_params.newton_global_chop  # Type of newton method (related to chopping strategy?)
-        self.params.newton_params = value_vector([0.2])  # Probably chop-criteria(?)
-        self.params.max_i_newton = 10
-
-        if self.discretizer_name == 'mech_discretizer':
-            self.params.tolerance_linear = 1e-10  # Tolerance for linear solver ||Ax - b||<tol_linslv
-            if self.reservoir.thermoporoelasticity:
-                self.params.linear_type = sim_params.cpu_superlu  # cpu_gmres_fs_cpr # cpu_superlu
-            else:
-                self.params.linear_type = sim_params.cpu_superlu  # cpu_gmres_fs_cpr # cpu_superlu
-            self.params.max_i_linear = 5000
-        elif self.discretizer_name == 'pm_discretizer':
-            ls1 = linear_solver_params()
-            ls1.linear_type = sim_params.cpu_superlu  # cpu_gmres_fs_cpr # cpu_superlu
-            ls1.tolerance_linear = 1.e-12
-            ls1.max_i_linear = 500
-            self.engine.ls_params.append(ls1)
-
     def set_reservoir(self):
         self.reservoir = UnstructReservoirCustom(timer=self.timer, idata=self.idata, discretizer=self.discretizer_name,
                                                  mode=self.mode, mesh_filename=self.mesh_filename)
