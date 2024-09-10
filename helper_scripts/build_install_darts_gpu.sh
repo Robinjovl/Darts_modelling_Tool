f5a57181bbfdb91c70029305658ffdd37bbc3b5b
Ilshat Saifullin <I.S.Saifullin@tudelft.nl>
2024-09-10 17:04:04 +0200

    bash script for compilation with GPU added

---
+++ helper_scripts/build_install_darts_gpu.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# compile discretizer using the Makefile (no GPU)
+cd discretizer
+make release -j 20 USE_OPENDARTS_LINEAR_SOLVERS=false 2>&1 | tee ../make_discretizer.log
+# sometimes the command above fails for file discretizer_build_info.cpp.in, so run it twice
+make release USE_OPENDARTS_LINEAR_SOLVERS=false | tee -a ../make_discretizer.log
+cd ..
+
+# need to link engines
+cd engines
+cp ../darts/discretizer.so .
+
+# get linear solvers binary compiled with GPU and include files
+cd lib
+mkdir darts_linear_solvers && cd darts_linear_solvers && mkdir lib && mkdir include && cd ..
+cp -r /oahu/data/open-darts-gitlab-runner-data/darts-linear-solvers-gpu/lib darts_linear_solvers/lib
+cp -r /oahu/data/open-darts-gitlab-runner-data/darts-linear-solvers-gpu/include darts_linear_solvers/include
+cd ..
+
+# compile engines using the Makefile
+make clean
+make gpu -j 20 USE_OPENDARTS_LINEAR_SOLVERS=false 2>&1 | tee ../make_engines.log
+cd ..
+
+# to add amgx shared library to wheels
+cp ../darts-linear-solvers/lib/AMGX/build/libamgxsh.so ./darts
+
+# build DARTS wheel
+./helper_scripts/build_install_darts.sh
+
