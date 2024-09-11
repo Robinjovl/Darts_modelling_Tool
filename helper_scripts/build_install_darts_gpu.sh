1b755166f1bacc4b050536929f0746be88a9f66d
Ilshat Saifullin <ilshat.junk@gmail.com>
2024-09-11 14:06:37 +0000

    Update build_install_darts_gpu.sh

--- helper_scripts/build_install_darts_gpu.sh
+++ helper_scripts/build_install_darts_gpu.sh
@@ -11,9 +11,9 @@ cd ../..
 # compile discretizer using the Makefile (no GPU)
 cd discretizer
 set +e # temporarily turn off set -e
-make release -j 20 USE_OPENDARTS_LINEAR_SOLVERS=false > ../make_discretizer.log
+make release -j 20 USE_OPENDARTS_LINEAR_SOLVERS=false 1>../make_discretizer_out.log 2>../make_discretizer_err.log
 # sometimes the command above fails for file discretizer_build_info.cpp.in, so run it twice
-make release USE_OPENDARTS_LINEAR_SOLVERS=false 2>&1 >> ../make_discretizer.log
+make release USE_OPENDARTS_LINEAR_SOLVERS=false 1>>../make_discretizer_out.log 2>>../make_discretizer_err.log
 cd ..
 
 # need to link engines
@@ -22,7 +22,7 @@ cp ../darts/discretizer.so .
 
 # compile engines using the Makefile
 make clean
-make gpu -j 20 USE_OPENDARTS_LINEAR_SOLVERS=false 2>&1 > ../make_engines.log
+make gpu -j 20 USE_OPENDARTS_LINEAR_SOLVERS=false 1>../make_engines_out.log 2>../make_engines_err.log
 cd ..
 
 # to add amgx shared library to wheels
