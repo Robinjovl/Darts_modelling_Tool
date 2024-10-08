354c4c96e7821ad7c2e8cdda666ffde380604ea8
Ilshat Saifullin <I.S.Saifullin@tudelft.nl>
2024-10-08 18:03:30 +0200

    use older code for cpg model and added the platform argument

--- models/cpg_sloping_fault/main.py
+++ models/cpg_sloping_fault/main.py
@@ -1,3 +1,5 @@
+from platform import platform
+
 from darts.engines import redirect_darts_output
 from model_cpg import Model
 
@@ -7,11 +9,10 @@ import matplotlib.pyplot as plt
 import time
 import os
 
-def run(physics_type : str, discr_type : str, case: str, out_dir: str, dt : float, n_time_steps : int, export_vtk=False,
-        platform='gpu'):
-    print('Test started', 'physics_type:', physics_type, 'discr_type:', discr_type, 'case:', case)
+def run(discr_type : str, case: str, out_dir: str, dt : float, n_time_steps : int, export_vtk=False, platform='cpu'):
+    print('Test started', 'discr_type:', discr_type, 'case:', case)
     redirect_darts_output(os.path.join(out_dir, 'run.log'))
-    m = Model(physics_type=physics_type, discr_type=discr_type, case=case, grid_out_dir=out_dir)
+    m = Model(discr_type=discr_type, case=case)
 
     m.init(output_folder=out_dir, platform=platform)
     m.save_data_to_h5(kind = 'solution')
@@ -49,32 +50,30 @@ def run(physics_type : str, discr_type : str, case: str, out_dir: str, dt : floa
     time_data_report.drop(columns=press_gridcells + chem_cols, inplace=True)
     # add time in years
     time_data_report['Time (years)'] = time_data_report['time'] / 365.25
-    writer = pd.ExcelWriter(os.path.join(out_dir, 'time_data_report_' + discr_type + '_' + physics_type + '.xlsx'))
+    writer = pd.ExcelWriter(os.path.join(out_dir, 'time_data_report_' + discr_type + '.xlsx'))
     time_data_report.to_excel(writer, sheet_name='time_data_report')
     writer.close()
 
-    failed, sim_time = check_performance_local(m, case, discr_type, physics_type)
-
-    return failed, sim_time, time_data_report
+    return time_data_report
 
 #####################################################
 
 def run_test(args: list = [], platform='cpu'):
     if len(args) > 1:
-        return test(case=args[0], physics_type=args[1], platform=platform)
+        return test(case=args[0], platform=platform)
     else:
         print('Not enough arguments provided')
         return 1, 0.0
 
-def test(case: str, physics_type : str, platform='cpu'):
+def test(case: str, platform='cpu'):
     dt = 365.25
     n_time_steps = 20
 
     export_vtk = True
 
-    #discr_types_list = ['cpg']  # cpg reservoir
-    #discr_types_list = ['struct']  # struct reservoir
-    discr_types_list = ['cpg', 'struct']  # both
+    #discr_types_list = ['cpp']  # cpg reservoir
+    #discr_types_list = ['python']  # struct reservoir
+    discr_types_list = ['cpp', 'python']  # both
 
     # test grids
     #cases_list = [43] # 10x10x10 sloping fault
@@ -99,62 +98,30 @@ def test(case: str, physics_type : str, platform='cpu'):
         for discr_type in discr_types_list:
             start = time.perf_counter()
             out_dir = discr_type + '_results_' + case
-            failed, sim_time, results[discr_type] = run(physics_type=physics_type, case=case,
-                                                        discr_type=discr_type, out_dir=out_dir,
-                                                        dt=dt, n_time_steps=n_time_steps, export_vtk=export_vtk,
-                                                        platform='cpu')
+            results[discr_type] = run(case=case, discr_type=discr_type, out_dir=out_dir, dt=dt, n_time_steps=n_time_steps,
+                                      export_vtk=export_vtk, platform=platform)
             end = time.perf_counter()
     elif mode == 'compare':
         for discr_type in discr_types_list:
             out_dir = discr_type + '_results_' + case
             results[discr_type] = pd.read_pickle(os.path.join(out_dir, 'time_data_' + discr_type + '.pkl'))
 
-    if physics_type == 'geothermal':
-        well_name = 'PRD'
-        col = well_name + ' : temperature'
-        plt.figure()
-        for k in results.keys():
-            y = np.array(results[k].filter(like=col)) - 273.15
-            t = np.array(results[k]['Time (years)'])  # to years
-            plt.plot(t, y, label=k)
-        plt.ylabel('Temperature prod well, C')
-        plt.xlabel('years')
-        plt.legend()
-        plt.savefig(os.path.join(out_dir, 'well_temperature_' + case + '.png'))
-        plt.close()
+    well_name = 'PRD'
+    col = well_name + ' : temperature'
+    plt.figure()
+    for k in results.keys():
+        y = np.array(results[k].filter(like=col)) - 273.15
+        t = np.array(results[k]['Time (years)'])  # to years
+        plt.plot(t, y, label=k)
+    plt.ylabel('Temperature prod well, C')
+    plt.xlabel('years')
+    plt.legend()
+    plt.savefig('well_temperature_' + case + '.png')
+    plt.close()
 
-    return failed, sim_time
-
-def check_performance_local(m, case, discr_type, physics_type):
-    import platform
-
-    os.makedirs('ref', exist_ok=True)
-
-    pkl_suffix = ''
-    if os.getenv('ODLS') != None and os.getenv('ODLS') == '-a':
-        pkl_suffix = '_iter'
-    file_name = os.path.join('ref', 'perf_' + platform.system().lower()[:3] + pkl_suffix +
-                             '_' + case + '_' + physics_type + '_' + discr_type + '.pkl')
-    overwrite = 0
-    if os.getenv('UPLOAD_PKL') == '1':
-        overwrite = 1
-
-    is_plk_exist = os.path.isfile(file_name)
-
-    failed = m.check_performance(perf_file=file_name, overwrite=overwrite, pkl_suffix=pkl_suffix)
-
-    if not is_plk_exist or overwrite == '1':
-        m.save_performance_data(file_name=file_name, pkl_suffix=pkl_suffix)
-        return False, 0.0
-
-    if is_plk_exist:
-        return (failed > 0), -1.0 #data[-1]['simulation time']
-    else:
-        return False, -1.0
+    return 0, 0.0
 
 if __name__ == '__main__':
-    physics_list = ['geothermal']#, 'dead_oil']
-    cases_list = ['generate_5x3x4', 'generate_51x51x1', 'case_40', 'case_43', 'case_40_actnum']
-    for physics_type in physics_list:
-        for case in cases_list:
-            test(case, physics_type)
+    cases_list = ['generate', '40', '43', '40_actnum']
+    for case in cases_list:
+        test(case)
