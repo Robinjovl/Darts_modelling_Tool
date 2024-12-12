d34a69b3380af6087e792caf0a9a3c30236a4ade
Sajjad Moslehi <s.moslehi@tudelft.nl>
2024-12-12 19:55:19 +0100

    Modify the Beta operator and add Lambda operator to class WellOperators in operator_evaluator.py to make it compatible with the reservoir operators.

--- models/2ph_comp/operator_evaluator.py
+++ models/2ph_comp/operator_evaluator.py
@@ -289,7 +289,7 @@ class WellOperators(OperatorsSuper):
         for j in self.property.ph:
             # fluid convective mass flux: x_cj [-] rho_mj [kmol/m3] k_rj [-] / mu_j [cP ∝ bar.day] (kmol/m3.bar.day)
             vec_values_as_np[self.FLUX_OP + j * self.ne:self.FLUX_OP + j * self.ne + self.nc_fl] = \
-                self.property.x[j][:self.nc_fl] * self.property.dens_m[j] * self.property.kr[j] / self.property.mu[j]
+                self.property.x[j][:self.nc_fl] * self.property.dens_m[j]
 
         """ Gamma operator for diffusion (same for thermal and isothermal) """
 
@@ -305,6 +305,11 @@ class WellOperators(OperatorsSuper):
 
         # E5_> porosity
         vec_values_as_np[self.PORO_OP] = 1.
+
+        """ Lambda operator for velocity calculations """
+        for j in self.property.ph:
+            # fluid mobility: k_rj [-] / mu_j [cP ∝ bar.day] (1/(bar.day))
+            vec_values_as_np[self.LAMBDA_OP + j] = self.property.kr[j] / self.property.mu[j]
 
 
         if self.thermal:
