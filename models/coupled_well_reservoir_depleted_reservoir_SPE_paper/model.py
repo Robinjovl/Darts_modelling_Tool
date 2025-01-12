32501b3da5e14957ea2109da0fd83ad47eec28dd
Sajjad Moslehi <s.moslehi@tudelft.nl>
2025-01-12 16:47:26 +0100

    Clean code

--- models/coupled_well_reservoir_depleted_reservoir_SPE_paper/model.py
+++ models/coupled_well_reservoir_depleted_reservoir_SPE_paper/model.py
@@ -61,39 +61,44 @@ class Model(CICDModel):
 
     def set_reservoir(self):
         (nr, nz) = (50, 5)
+        (dr, dz) = (5, 50)
         if 1:
             case = 'depleted_gas_reservoir'
             poro = np.ones((nr, nz)) * 0.2
-            perm = np.ones((nr, nz)) * 20
+            permr = np.ones((nr, nz)) * 20
+
+            permz = permr / 10
 
         else:
             case = 'Porthos'
             poro = np.ones((nr, nz)) * 0.075
-            perm = np.ones((nr, nz)) * 0.29
+            permr = np.ones((nr, nz)) * 0.29
             # upper detfurth
-            perm[:, :16] = 12.6
+            permr[:, :16] = 12.6
             # hardegsen
             poro[:, :10] = 0.09
-            perm[:, :10] = 24
+            permr[:, :10] = 24
             # hardegsen high perm
             poro[:, :8] = 0.2
-            perm[:, :8] = 240
+            permr[:, :8] = 240
             # hardegsen
             poro[:, :6] = 0.09
-            perm[:, :6] = 24
+            permr[:, :6] = 24
             # caprock
             poro[:, :4] = 0.01
-            perm[:, :4] = 0.01
+            permr[:, :4] = 0.01
 
-        self.set_reservoir_radial(nr=nr, dr=5, nz=nz, dz=50, poro=poro.flatten(order='F'), perm=perm.flatten(order='F'))
+            permz = permr / 10
+
+        self.set_reservoir_radial(nr=nr, dr=dr, nz=nz, dz=dz, poro=poro.flatten(order='F'), permr=permr.flatten(order='F'), permz=permz.flatten(order='F'))
 
         return
 
-    def set_reservoir_radial(self, nr, dr, nz, dz, poro, perm):
+    def set_reservoir_radial(self, nr, dr, nz, dz, poro, permr, permz):
         self.seg_ratio = 1
         if 1:
             from nearwellbore import RadialStruct
-            self.reservoir = RadialStruct(self.timer, nr=nr, nz=nz, dr=dr, dz=dz, permr=perm, permz=perm/10, poro=poro,
+            self.reservoir = RadialStruct(self.timer, nr=nr, nz=nz, dr=dr, dz=dz, permr=permr, permz=permz, poro=poro,
                                           R1=1000, logspace=True, boundary_volume=4.3e10, depth=1775)
 
         else:
@@ -104,81 +109,6 @@ class Model(CICDModel):
         # self.reservoir.boundary_volumes['xy_minus'] = 1e8
 
         return
-
-    def set_wells(self):
-        """================================================= Well 1 ================================================="""
-        well_1_name = "I1"
-        well_1_type = "ms_well"
-        # Lengths of the well segments are specified here.
-        # The lengths of the well segments in front of the reservoir must be equal to the height of the reservoir cells.
-        # well_1_segments_lengths = np.concatenate(([50], 50 * np.ones(20), [50]))  # From bottom to top of the wellbore
-        # well_1_segments_lengths = 10 * np.ones(2)
-        well_1_segments_lengths = 50 * np.ones(40)
-        well_1_ID = 0.1
-        well_1_inclination_angle = 0  # in degrees relative to the vertical direction
-        well_1_wall_roughness = 2.5e-5
-        verbose = True
-        well_1_geometry = PipeGeometry(well_1_name, well_1_segments_lengths, well_1_ID, well_1_inclination_angle,
-                                       well_1_wall_roughness, verbose)
-
-        # %% Set initial conditions in the pipe using SingleAmbientTemperature
-        system_temperature = self.physics.property_containers[0].temperature
-        pipe_head_pressure = 20   # bar
-        pipe_head_segment_index = well_1_geometry.num_segments - 1  # index starts from zero
-
-        # Wellhead conditions because of the constant rate control
-        # zero = self.physics.axes_min[1]
-        # well_head_segment_phase = 'gas'
-        # well_head_segment_composition = [1.0 - 2 * zero*10, zero*10, zero*10]
-        # well_head_segment_interval = [well_1_geometry.pipe_length - 50, well_1_geometry.pipe_length]
-        initial_fluid_conditions = {'phases_names': ['gas'], 'phases_compositions': [[1e-5, 1 - 2 * 1e-5, 1e-5]],
-                                    'pipe_intervals': [[0, well_1_geometry.pipe_length]]}  # 0 is the beginning of the pipe
-        # initial_fluid_conditions = {'phases_names': ['liquid'], 'phases_compositions': [[1e-5, 1 - 1e-5]],
-        #                             'pipe_intervals': [[0, well_1_geometry.pipe_length]]}  # 0 is the beginning of the pipe
-
-        well_1_initial_conditions = SingleAmbientTemperature(well_1_name, well_1_geometry, self.physics.property_containers[0], system_temperature,
-                                                             pipe_head_pressure, pipe_head_segment_index,
-                                                             initial_fluid_conditions, verbose)
-
-        # %% Put initial conditions in wells_initial_conditions
-        initial_CO2_mole_fraction = [initial_fluid_conditions['phases_compositions'][0][0]] * well_1_geometry.num_segments
-        initial_C1_mole_fraction = [initial_fluid_conditions['phases_compositions'][0][1]] * well_1_geometry.num_segments
-
-        self.wells_initial_conditions = {'initial_pressure': well_1_initial_conditions.p_init_segments,
-                                         'initial_CO2_mole_fraction': initial_CO2_mole_fraction,
-                                         'initial_C1_mole_fraction': initial_C1_mole_fraction}
-        # self.wells_initial_conditions = {'initial_pressure': well_1_initial_conditions.p_init_segments,
-        #                                  'initial_CO2_mole_fraction': initial_CO2_mole_fraction}
-        check_initial_conditions(self.wells_initial_conditions, self.physics.property_containers[0].components_name,
-                                 not self.physics.property_containers[0].thermal)
-
-        # self.reservoir.add_well(well_1_name, well_1_type, well_geometry=well_1_geometry, physics=self.physics, darts_model = self)
-        # reservoir_middle_cell_index = int(self.reservoir.nx / 2)
-        # self.reservoir.add_perforation(well_1_name, perforated_reservoir_cell_index=(1, 1, 1), perforated_segment_index=2, well_geometry=well_1_geometry, ms_well=True)
-        # self.reservoir.add_perforation(well_1_name, perforated_reservoir_cell_index=(1, 1, 2), perforated_segment_index=3, well_geometry=well_1_geometry, ms_well=True)
-
-        self.reservoir.add_well(well_1_name, well_1_type, well_geometry=well_1_geometry, physics=self.physics, darts_model = self)
-        self.reservoir.add_perforation(well_1_name, cell_index=(1, 1, 3), well_geometry=well_1_geometry)
-        # self.reservoir.add_perforation(well_1_name, perforated_reservoir_cell_index=(1, 1, 2), perforated_segment_index=well_1_geometry.num_segments - self.reservoir.nz + 4, well_geometry=well_1_geometry, ms_well=True)
-        # self.reservoir.add_perforation(well_1_name, perforated_reservoir_cell_index=(1, 1, 5), perforated_segment_index=well_1_geometry.num_segments - self.reservoir.nz + 5, well_geometry=well_1_geometry, ms_well=True)
-        # self.reservoir.add_perforation(well_1_name, perforated_reservoir_cell_index=(1, 1, 5), perforated_segment_index=well_1_geometry.num_segments - self.reservoir.nz + 5, well_geometry=well_1_geometry, ms_well=True)
-        # self.reservoir.add_perforation(well_1_name, perforated_reservoir_cell_index=(1, 1, 5), perforated_segment_index=well_1_geometry.num_segments - self.reservoir.nz + 5, well_geometry=well_1_geometry, ms_well=True)
-        # for k in range(3, self.reservoir.nz):
-        #     self.reservoir.add_perforation(well_1_name, perforated_reservoir_cell_index=(1, 1, k + 1), perforated_segment_index=well_1_geometry.num_segments - self.reservoir.nz + k + 1, well_geometry=well_1_geometry, ms_well=True)
-
-        """================================================= Well 2 ================================================="""
-        # well_2_name = "P1"
-        # well_2_type = "basic_well"
-        # well_2_ID = 0.1
-        # self.reservoir.add_well(well_2_name, well_2_type, well_ID=well_2_ID)
-        # self.reservoir.add_perforation(well_2_name, cell_index=(1, 1, 1), well_ID=well_2_ID)
-
-        """================================================= Well 3 ================================================="""
-        # well_3_name = "P2"
-        # well_3_type = "basic_well"
-        # well_3_ID = 0.1
-        # self.reservoir.add_well(well_3_name, well_3_type, well_ID=well_3_ID)
-        # self.reservoir.add_perforation(well_3_name, cell_index=(self.reservoir.nx, 1, 1), well_ID=well_3_ID)
 
     def set_physics(self):
         # """Physical properties"""
@@ -257,6 +187,81 @@ class Model(CICDModel):
                                            "miu_aqueous_phase": lambda: self.physics.property_containers[0].mu[1]}
 
         return
+
+    def set_wells(self):
+        """================================================= Well 1 ================================================="""
+        well_1_name = "I1"
+        well_1_type = "ms_well"
+        # Lengths of the well segments are specified here.
+        # The lengths of the well segments in front of the reservoir must be equal to the height of the reservoir cells.
+        # well_1_segments_lengths = np.concatenate(([50], 50 * np.ones(20), [50]))  # From bottom to top of the wellbore
+        # well_1_segments_lengths = 10 * np.ones(2)
+        well_1_segments_lengths = 50 * np.ones(40)
+        well_1_ID = 0.1
+        well_1_inclination_angle = 0  # in degrees relative to the vertical direction
+        well_1_wall_roughness = 2.5e-5
+        verbose = True
+        well_1_geometry = PipeGeometry(well_1_name, well_1_segments_lengths, well_1_ID, well_1_inclination_angle,
+                                       well_1_wall_roughness, verbose)
+
+        # %% Set initial conditions in the pipe using SingleAmbientTemperature
+        system_temperature = self.physics.property_containers[0].temperature
+        pipe_head_pressure = 20   # bar
+        pipe_head_segment_index = well_1_geometry.num_segments - 1  # index starts from zero
+
+        # Wellhead conditions because of the constant rate control
+        # zero = self.physics.axes_min[1]
+        # well_head_segment_phase = 'gas'
+        # well_head_segment_composition = [1.0 - 2 * zero*10, zero*10, zero*10]
+        # well_head_segment_interval = [well_1_geometry.pipe_length - 50, well_1_geometry.pipe_length]
+        initial_fluid_conditions = {'phases_names': ['gas'], 'phases_compositions': [[1e-5, 1 - 2 * 1e-5, 1e-5]],
+                                    'pipe_intervals': [[0, well_1_geometry.pipe_length]]}  # 0 is the beginning of the pipe
+        # initial_fluid_conditions = {'phases_names': ['liquid'], 'phases_compositions': [[1e-5, 1 - 1e-5]],
+        #                             'pipe_intervals': [[0, well_1_geometry.pipe_length]]}  # 0 is the beginning of the pipe
+
+        well_1_initial_conditions = SingleAmbientTemperature(well_1_name, well_1_geometry, self.physics.property_containers[0], system_temperature,
+                                                             pipe_head_pressure, pipe_head_segment_index,
+                                                             initial_fluid_conditions, verbose)
+
+        # %% Put initial conditions in wells_initial_conditions
+        initial_CO2_mole_fraction = [initial_fluid_conditions['phases_compositions'][0][0]] * well_1_geometry.num_segments
+        initial_C1_mole_fraction = [initial_fluid_conditions['phases_compositions'][0][1]] * well_1_geometry.num_segments
+
+        self.wells_initial_conditions = {'initial_pressure': well_1_initial_conditions.p_init_segments,
+                                         'initial_CO2_mole_fraction': initial_CO2_mole_fraction,
+                                         'initial_C1_mole_fraction': initial_C1_mole_fraction}
+        # self.wells_initial_conditions = {'initial_pressure': well_1_initial_conditions.p_init_segments,
+        #                                  'initial_CO2_mole_fraction': initial_CO2_mole_fraction}
+        check_initial_conditions(self.wells_initial_conditions, self.physics.property_containers[0].components_name,
+                                 not self.physics.property_containers[0].thermal)
+
+        # self.reservoir.add_well(well_1_name, well_1_type, well_geometry=well_1_geometry, physics=self.physics, darts_model = self)
+        # reservoir_middle_cell_index = int(self.reservoir.nx / 2)
+        # self.reservoir.add_perforation(well_1_name, perforated_reservoir_cell_index=(1, 1, 1), perforated_segment_index=2, well_geometry=well_1_geometry, ms_well=True)
+        # self.reservoir.add_perforation(well_1_name, perforated_reservoir_cell_index=(1, 1, 2), perforated_segment_index=3, well_geometry=well_1_geometry, ms_well=True)
+
+        self.reservoir.add_well(well_1_name, well_1_type, well_geometry=well_1_geometry, physics=self.physics, darts_model = self)
+        self.reservoir.add_perforation(well_1_name, cell_index=(1, 1, 3), well_geometry=well_1_geometry)
+        # self.reservoir.add_perforation(well_1_name, perforated_reservoir_cell_index=(1, 1, 2), perforated_segment_index=well_1_geometry.num_segments - self.reservoir.nz + 4, well_geometry=well_1_geometry, ms_well=True)
+        # self.reservoir.add_perforation(well_1_name, perforated_reservoir_cell_index=(1, 1, 5), perforated_segment_index=well_1_geometry.num_segments - self.reservoir.nz + 5, well_geometry=well_1_geometry, ms_well=True)
+        # self.reservoir.add_perforation(well_1_name, perforated_reservoir_cell_index=(1, 1, 5), perforated_segment_index=well_1_geometry.num_segments - self.reservoir.nz + 5, well_geometry=well_1_geometry, ms_well=True)
+        # self.reservoir.add_perforation(well_1_name, perforated_reservoir_cell_index=(1, 1, 5), perforated_segment_index=well_1_geometry.num_segments - self.reservoir.nz + 5, well_geometry=well_1_geometry, ms_well=True)
+        # for k in range(3, self.reservoir.nz):
+        #     self.reservoir.add_perforation(well_1_name, perforated_reservoir_cell_index=(1, 1, k + 1), perforated_segment_index=well_1_geometry.num_segments - self.reservoir.nz + k + 1, well_geometry=well_1_geometry, ms_well=True)
+
+        """================================================= Well 2 ================================================="""
+        # well_2_name = "P1"
+        # well_2_type = "basic_well"
+        # well_2_ID = 0.1
+        # self.reservoir.add_well(well_2_name, well_2_type, well_ID=well_2_ID)
+        # self.reservoir.add_perforation(well_2_name, cell_index=(1, 1, 1), well_ID=well_2_ID)
+
+        """================================================= Well 3 ================================================="""
+        # well_3_name = "P2"
+        # well_3_type = "basic_well"
+        # well_3_ID = 0.1
+        # self.reservoir.add_well(well_3_name, well_3_type, well_ID=well_3_ID)
+        # self.reservoir.add_perforation(well_3_name, cell_index=(self.reservoir.nx, 1, 1), well_ID=well_3_ID)
 
     def set_well_controls(self):
         inj_stream = [1e-5, 1e-5]
