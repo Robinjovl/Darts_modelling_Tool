9a469c4bfe504ded8e2f376a162a7c101986e4ed
Florian Delprat <shaitan.lyssm@gmail.com>
2025-01-18 04:32:40 +0100

    Exclude logging folder

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages, Distribution
 # custom class to inform setuptools about self-compiled extensions in the distribution
 # and hence enforce it to create platform wheel
 class BinaryDistribution(Distribution):
-    def has_ext_modules(foo):
+    def has_ext_modules(self):
         return True
 
 setup(
     # Add packages that are inside folder darts-package
     packages = find_packages(
     	where = '.',
-        exclude = ['discretizer', 'engines', 'models', 'solvers', 'docs', 'thirdparty']),
+        exclude = ['discretizer', 'engines', 'models', 'solvers', 'docs', 'thirdparty', 'logging']),
 
     # Now only include already built libraries, and build_info, otherwise it will not find the file when using darts.
     package_data={'darts': ['*.pyd', '*.so', '*.dll', 'build_info.txt']},
