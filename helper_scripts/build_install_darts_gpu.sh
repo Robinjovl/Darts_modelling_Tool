81e94135d9606f76aa82a4c7371bf5b7dbe35e32
Florian Delprat <shaitan.lyssm@gmail.com>
2025-01-21 15:47:51 +0100

    Fix path to amgx lib

--- helper_scripts/build_install_darts_gpu.sh
+++ helper_scripts/build_install_darts_gpu.sh
@@ -2,26 +2,21 @@
 set -e
 
 # get linear solvers binary compiled with GPU and include files
-# cd engines/lib
-# rm -rf darts_linear_solvers
-# mkdir darts_linear_solvers && cd darts_linear_solvers && mkdir lib && mkdir include && cd ..
 
 if [[ "$GSELINSOLVERSPATH" == "" ]]; then
   echo "Error: the environment variable GSELINSOLVERSPATH is not defined!"
   exit 1
 fi
-# cp -r $GSELINSOLVERSPATH/lib darts_linear_solvers
-# cp -r $GSELINSOLVERSPATH/include darts_linear_solvers
-# cd ../..
 
 mkdir -p build
 cd build
+rm -f CMakeCache.txt 
 cmake .. -DBOS_SOLVERS_DIR=$GSELINSOLVERSPATH -DOPENDARTS_CONFIG=GPU
 make install
 cd ..
 
 # to add amgx shared library to wheels
-cp -v ./engines/lib/darts_linear_solvers/lib/libamgxsh.so ./darts
+cp -v $GSELINSOLVERSPATH/lib/libamgxsh.so ./darts
 
 # build DARTS wheel
 ./helper_scripts/build_install_darts.sh
