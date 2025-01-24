d90ef3971376f7c27a149434244766589f301041
Florian Delprat <shaitan.lyssm@gmail.com>
2025-01-25 00:52:44 +0100

    Fix libstdc++ bundling

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         exclude = ['discretizer', 'engines', 'models', 'solvers', 'docs', 'thirdparty']),
 
     # Now only include already built libraries, and build_info, otherwise it will not find the file when using darts.
-    package_data={'darts': ['*.pyd', '*.so', '*.dll', 'build_info.txt']},
+    package_data={'darts': ['*.pyd', '*.so', '*.dll', 'build_info.txt', 'libstdc++.so.6']},
 
     # Package metadata
     description='Delft Advanced Research Terra Simulator',
