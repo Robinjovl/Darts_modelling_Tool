3e931f50df240d7897830685a38860ff831888e5
Lyss <shaitan.lyssm@gmail.com>
2025-01-28 01:51:35 +0100

    Change default logging level to timer

--- logging/src/logging.h
+++ logging/src/logging.h
@@ -105,7 +105,7 @@ using enum LoggingLevel;
  * The default verbosity level is information because users are not interested
  * in debug information.
  */
-constexpr LoggingLevel DEFAULT_LOGGING_LEVEL = LoggingLevel::INFO;
+constexpr LoggingLevel DEFAULT_LOGGING_LEVEL = TIMER;
 
 struct FStreamWithMutex {
   FStreamWithMutex() {}
