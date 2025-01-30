f731cbe5d2f75bf02034c2169e01c96467a4ae77
Florian Delprat <shaitan.lyssm@gmail.com>
2025-01-30 02:00:39 +0100

    Fix typo

--- setup.py
+++ setup.py
@@ -14,9 +14,6 @@ setup(
 
     # Now only include already built libraries, and build_info, otherwise it will not find the file when using darts.
     package_data={'darts': ['*.pyd', '*.so', '*.dll', 'build_info.txt', 'lib/*.so', 'lib/*.dll', 'lib/*.pyd', 'libstdc++.so.6']},
-    entry_points = {
-        'console_scripts': ['darts = darts.tools.cli:main',],
-    },
 
     # Add darts command line interface
     entry_points = {
