0e94994f183a0a1cbbd5f07a5334a1da7e63d6b2
Sajjad_Moslehi <sajjadmoslehi127@gmail.com>
2025-02-17 16:39:22 +0100

    Correct the calculation of rate operator index in well_controls.cpp

--- engines/src/well_controls.cpp
+++ engines/src/well_controls.cpp
@@ -54,7 +54,7 @@ int well_control_iface::add_to_jacobian(value_t dt, index_t well_head_idx, value
 	state.assign(X.begin() + (well_head_idx + well_state_offset) * n_block_size + P_VAR, X.begin() + (well_head_idx + well_state_offset) * n_block_size + P_VAR + n_state_size);
     well_controls_etor->evaluate_with_derivatives(state, block_idx, well_control_ops, well_control_ops_derivs);
 	value_t p_diff = X_well_head[0] - X_well_body[0];
-	index_t rate_op_idx = n_state_size + this->control_type * n_phases + phase_idx;  // find correct index in WellControlOperators
+	index_t rate_op_idx = n_state_size + (this->control_type - 1) * n_phases + phase_idx;  // find correct index in WellControlOperators
 
 	// RHS
 	RHS_well_head[0] = well_control_ops[rate_op_idx] * p_diff * segment_trans - well_control_spec[0];
@@ -124,7 +124,7 @@ int well_control_iface::check_constraint_violation(value_t dt, index_t well_head
   else
   {
 	// Check if rate constraint is violated
-	index_t rate_op_idx = n_state_size + this->control_type * n_phases + phase_idx;  // find correct index in WellControlOperators
+	index_t rate_op_idx = n_state_size + (this->control_type - 1) * n_phases + phase_idx;  // find correct index in WellControlOperators
 	int n_ops = n_state_size + 4 * n_phases;
     well_control_ops.resize(n_ops);
 
