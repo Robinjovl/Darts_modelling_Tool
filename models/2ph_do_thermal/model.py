4850990a0d02ec740601760f05a310b19bad5a1a
Michiel Wapperom <m.b.wapperom@tudelft.nl>
2025-02-18 19:07:43 +0100

    Set target phase 'wat'

--- models/2ph_do_thermal/model.py
+++ models/2ph_do_thermal/model.py
@@ -86,7 +86,7 @@ class Model(CICDModel):
         for i, w in enumerate(self.reservoir.wells):
             if i == 0:
                 w.control = self.physics.define_well_controls(well_name=w.name, control_type=well_control_iface.MOLAR_RATE,
-                                                              is_inj=True, target=5., phase_name='oil', inj_stream=self.inj[:-1], inj_temp=self.inj[-1])
+                                                              is_inj=True, target=5., phase_name='wat', inj_stream=self.inj[:-1], inj_temp=self.inj[-1])
             else:
                 w.control = self.physics.define_well_controls(well_name=w.name, control_type=well_control_iface.BHP,
                                                               is_inj=False, target=180.)
