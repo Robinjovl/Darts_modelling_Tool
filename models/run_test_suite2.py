586bff14bbc0a7099ed3ee15df64a54843b7b14c
Ilshat Saifullin <13548357-i-l-s-h-a-t@users.noreply.gitlab.com>
2025-02-24 13:31:12 +0000

    Edit run_test_suite2.py - get back Chem_benchmark_new

--- models/run_test_suite2.py
+++ models/run_test_suite2.py
@@ -12,7 +12,7 @@ def run_testing(platform, overwrite, iter_solvers, test_all_models):
                      '2ph_geothermal', '2ph_geothermal_mass_flux',
                      '3ph_comp_w', '3ph_do', '3ph_bo',
                      'Uniform_Brugge',
-                     #'Chem_benchmark_new',
+                     'Chem_benchmark_new',
                      'CCS',
                      #'CO2_foam_CCS',
                      'SPE11b',
