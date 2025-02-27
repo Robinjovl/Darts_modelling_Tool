a6823409b516705c4c35e49240aab58a13cd56f1
Sajjad Moslehi <s.moslehi@tudelft.nl>
2025-02-27 19:14:32 +0100

    Modify save_results.py for thermal scenarios

--- darts/wells/save_results.py
+++ darts/wells/save_results.py
@@ -20,6 +20,7 @@ def save_segments_primary_vars_and_phase_props(h5_well_data, coupled_model):
 
     p = np.zeros(num_segments)
     z = np.zeros((num_segments, property_container.nc - 1))
+    T = np.zeros((num_segments))
 
     sG = np.zeros(num_segments)
     sL = np.zeros(num_segments)
@@ -36,7 +37,12 @@ def save_segments_primary_vars_and_phase_props(h5_well_data, coupled_model):
         for j in range(num_segments):
             state = h5_well_data["dynamic"]["X"][i,j + num_perfs,:]
             p[j] = state[0]
-            z[j,:] = state[1:]
+
+            if not coupled_model.physics.property_containers[0].thermal:
+                z[j, :] = state[1:]
+            elif coupled_model.physics.property_containers[0].thermal:
+                z[j, :] = state[1:-1]
+                T[j] = state[-1]
 
             property_container.evaluate(state)
             xG[j,:] = property_container.x[0,:]
@@ -48,10 +54,10 @@ def save_segments_primary_vars_and_phase_props(h5_well_data, coupled_model):
             miuG[j] = property_container.mu[0]
             miuL[j] = property_container.mu[1]
 
-        ts_primary_vars_and_phases_props = [p.copy(), z.copy(), xG.copy(), xL.copy(),
+        ts_primary_vars_and_phases_props = [p.copy(), z.copy(), T.copy(), xG.copy(), xL.copy(),
                                             sG.copy(), sL.copy(), rhoG.copy(), rhoL.copy(),
                                             miuG.copy(), miuL.copy()]
         data_frame = pd.concat([data_frame, pd.DataFrame(list(zip(*ts_primary_vars_and_phases_props)),
-                                                         columns=["Pressure", "Overall mole fractions", "xG", "xL",
-                                                                  "sG", "sL", "rhoG", "rhoL", "miuG", "miuL"])])
+                                                         columns=["Pressure", "Overall mole fractions", "Temperature",
+                                                                  "xG", "xL", "sG", "sL", "rhoG", "rhoL", "miuG", "miuL"])])
     data_frame.to_pickle("output/stored_primary_vars_and_phase_props.pkl")
