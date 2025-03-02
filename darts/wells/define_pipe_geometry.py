8714cd2ba6ad1dcede7bb6c018a151ec917d4b4f
Sajjad_Moslehi <sajjadmoslehi127@gmail.com>
2025-03-02 17:06:13 +0100

    Clean define_pipe_geometry.py

--- darts/wells/define_pipe_geometry.py
+++ darts/wells/define_pipe_geometry.py
@@ -60,8 +60,10 @@ class PipeGeometry:
         self.z_p = self.z[1::1]
 
         self.D = self.z_p - self.z_m   # Distances between the centroids of neighboring interfaces
-        self.D = np.insert(self.D, 0, self.D[0])  # Duplicate the first value of the array, which will be used for exterface
-        self.D = np.append(self.D, self.D[-1])  # Duplicate the last value of the array, which will be used for exterface
+
+        # Duplicate the first and last values of the array, which will be used for exterfaces
+        self.D = np.insert(self.D, 0, self.D[0])
+        self.D = np.append(self.D, self.D[-1])
 
         # Get interfaces positions
         self.z_interfaces = np.cumsum(self.segments_lengths)[:-1]
