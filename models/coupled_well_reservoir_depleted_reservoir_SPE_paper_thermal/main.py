9132d34e547cc88156c915a22ed3fdf187ce4145
Sajjad Moslehi <s.moslehi@tudelft.nl>
2025-03-05 23:50:34 +0100

    Add visualize_results_line_graphs to the main file of the thermal coupled scenario

--- models/coupled_well_reservoir_depleted_reservoir_SPE_paper_thermal/main.py
+++ models/coupled_well_reservoir_depleted_reservoir_SPE_paper_thermal/main.py
@@ -12,6 +12,7 @@ from darts.engines import redirect_darts_output
 from darts.tools.hdf5_tools import load_hdf5_to_dict
 from darts.wells.save_results import save_segments_primary_vars_and_phase_props
 from darts.wells.visualize_results_heat_maps import visualize_results_heat_maps
+from darts.wells.visualize_results_line_graphs import visualize_results_line_graphs
 
 redirect_darts_output('run_log.log')
 coupled_model = Model()
@@ -66,4 +67,5 @@ else:
     save_segments_primary_vars_and_phase_props(h5_well_data, coupled_model)
 
     primary_vars_and_phase_props_file_address = "output/stored_primary_vars_and_phase_props.pkl"
-    visualize_results_heat_maps(primary_vars_and_phase_props_file_address, h5_well_data, coupled_model)
+    # visualize_results_heat_maps(primary_vars_and_phase_props_file_address, h5_well_data, coupled_model)
+    visualize_results_line_graphs(primary_vars_and_phase_props_file_address, h5_well_data, coupled_model, 3)
