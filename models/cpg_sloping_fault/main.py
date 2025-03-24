6056dbb9f58057585b33d7d8e6d793966e901afc
Ilshat Saifullin <I.S.Saifullin@tudelft.nl>
2025-03-24 20:06:40 +0100

    faultfile case in the main.py

--- models/cpg_sloping_fault/main.py
+++ models/cpg_sloping_fault/main.py
@@ -233,23 +233,24 @@ if __name__ == '__main__':
             platform = 'gpu'
 
     physics_list = []
-    #physics_list += ['geothermal']
+    physics_list += ['geothermal']
     #physics_list += ['deadoil']
-    physics_list += ['ccs']
+    #physics_list += ['ccs']
 
     cases_list = []
     #cases_list += ['generate_5x3x4']
-    cases_list += ['generate_51x51x1']
+    #cases_list += ['generate_51x51x1']
+    cases_list += ['generate_51x51x1_faultmult']
     #cases_list += ['generate_100x100x100']
     #cases_list += ['case_40x40x10']
     #cases_list += ['brugge']
 
     well_controls = []
     #well_controls += ['wrate']
-    #well_controls += ['wbhp']
+    well_controls += ['wbhp']
     #well_controls += ['wperiodic']
     #well_controls += ['co2_wbhp']
-    well_controls += ['co2_wrate']
+    #well_controls += ['co2_wrate']
 
     for physics_type in physics_list:
         for case_geom in cases_list:
