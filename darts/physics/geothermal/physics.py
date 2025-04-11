ce0309fe9ff13c429f923141540a2985350b40ad
Michiel Wapperom <m.b.wapperom@tudelft.nl>
2025-04-11 14:07:11 +0200

    Use mesh.n_res_blocks instead of mesh.n_blocks in Physics.set_initial_conditions_*()

--- darts/physics/geothermal/physics.py
+++ darts/physics/geothermal/physics.py
@@ -147,7 +147,7 @@ class Geothermal(PhysicsBase):
         depths = np.asarray(mesh.depth)
 
         # adjust the size of initial_state array in c++
-        mesh.initial_state.resize(mesh.n_blocks * self.n_vars)
+        mesh.initial_state.resize(mesh.n_res_blocks * self.n_vars)
 
         # Loop over variables to fill initial_state vector in c++
         for ith_var, variable in enumerate(self.vars):
@@ -159,8 +159,8 @@ class Geothermal(PhysicsBase):
                 t_itor = interp1d(input_depth, input_distribution['temperature'], kind='linear', fill_value='extrapolate')
                 temperature = t_itor(depths)
 
-                values = np.empty(mesh.n_blocks)
-                for j in range(mesh.n_blocks):
+                values = np.empty(mesh.n_res_blocks)
+                for j in range(mesh.n_res_blocks):
                     state = np.array([pressure[j], temperature[j]])
                     values[j] = self.property_containers[0].compute_total_enthalpy(state, temperature[j])
             else:
@@ -181,23 +181,23 @@ class Geothermal(PhysicsBase):
         for variable, values in input_distribution.items():
             if not np.isscalar(values) and not len(values) == mesh.n_res_blocks:
                 warnings.warn('Initial condition for variable {} has different length, resizing {} to {}'.
-                              format(variable, len(values), mesh.n_res_blocks))
+                              format(variable, len(values), mesh.n_res_blocks), stacklevel=2)
                 input_distribution[variable] = np.resize(np.asarray(values), mesh.n_res_blocks)
 
         # adjust the size of initial_state array in c++
-        mesh.initial_state.resize(mesh.n_blocks * self.n_vars)
+        mesh.initial_state.resize(mesh.n_res_blocks * self.n_vars)
 
         # set initial pressure
         np.asarray(mesh.initial_state)[0::self.n_vars] = input_distribution['pressure']
 
         # interpolate pressure and temperature to compute enthalpies
-        enthalpy = np.empty(mesh.n_blocks)
+        enthalpy = np.empty(mesh.n_res_blocks)
         if 'enthalpy' in input_distribution.keys():
-            enth = np.ones(mesh.n_blocks) * input_distribution['enthalpy'] if not np.isscalar(input_distribution['enthalpy']) else input_distribution['enthalpy']
+            enth = np.ones(mesh.n_res_blocks) * input_distribution['enthalpy'] if not np.isscalar(input_distribution['enthalpy']) else input_distribution['enthalpy']
             enthalpy[:] = enth
         elif not np.isscalar(input_distribution['pressure']):
             # Pressure specified as an array
-            for j in range(mesh.n_blocks):
+            for j in range(mesh.n_res_blocks):
                 state = value_vector([input_distribution['pressure'][j], 0])
                 temp = input_distribution['temperature'][j] if not np.isscalar(input_distribution['temperature']) else input_distribution['temperature']
                 enthalpy[j] = self.property_containers[0].compute_total_enthalpy(state, temp)
--- darts/physics/super/physics.py
+++ darts/physics/super/physics.py
@@ -177,7 +177,7 @@ class Compositional(PhysicsBase):
         depths = np.asarray(mesh.depth)
 
         # adjust the size of initial_state array in c++
-        mesh.initial_state.resize(mesh.n_blocks * self.n_vars)
+        mesh.initial_state.resize(mesh.n_res_blocks * self.n_vars)
 
         # Loop over variables to fill initial_state vector in c++
         for ith_var, variable in enumerate(self.vars):
@@ -203,7 +203,7 @@ class Compositional(PhysicsBase):
                 itor = interp1d(input_depth, input_distribution[variable], kind='linear', fill_value='extrapolate')
                 values = itor(depths)
 
-            values = np.resize(np.asarray(values), mesh.n_blocks)
+            values = np.resize(np.asarray(values), mesh.n_res_blocks)
             np.asarray(mesh.initial_state)[ith_var::self.n_vars] = values
 
     def set_initial_conditions_from_array(self, mesh: conn_mesh, input_distribution: dict):
@@ -217,11 +217,11 @@ class Compositional(PhysicsBase):
         for variable, values in input_distribution.items():
             if not np.isscalar(values) and not len(values) == mesh.n_res_blocks:
                 warnings.warn('Initial condition for variable {} has different length, resizing {} to {}'.
-                              format(variable, len(values), mesh.n_res_blocks))
+                              format(variable, len(values), mesh.n_res_blocks), stacklevel=2)
                 input_distribution[variable] = np.resize(np.asarray(values), mesh.n_res_blocks)
 
         # adjust the size of initial_state array in c++
-        mesh.initial_state.resize(mesh.n_blocks * self.n_vars)
+        mesh.initial_state.resize(mesh.n_res_blocks * self.n_vars)
 
         # set initial pressure
         np.asarray(mesh.initial_state)[0::self.n_vars] = input_distribution['pressure']
@@ -232,10 +232,10 @@ class Compositional(PhysicsBase):
                 np.asarray(mesh.initial_state)[(self.n_vars - 1)::self.n_vars] = input_distribution['temperature']
             else:
                 # interpolate pressure and temperature to compute enthalpies
-                enthalpy = np.empty(mesh.n_blocks)
+                enthalpy = np.empty(mesh.n_res_blocks)
                 if not np.isscalar(input_distribution['pressure']):
                     # Pressure specified as an array
-                    for j in range(mesh.n_blocks):
+                    for j in range(mesh.n_res_blocks):
                         state = value_vector([input_distribution['pressure'][j], 0])
                         temp = input_distribution['temperature'][j] if not np.isscalar(input_distribution['temperature']) else input_distribution['temperature']
                         enthalpy[j] = self.property_containers[0].compute_total_enthalpy(state, temp)
