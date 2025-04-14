743bc1b22a1d51714d273e3a55c7a40eb0e60dd8
Sajjad Moslehi <s.moslehi@tudelft.nl>
2025-04-14 22:12:17 +0200

    Shorten time_steps for coupled_well_reservoir_aquifer_2_perfs

--- models/coupled_well_reservoir_aquifer_2_perfs/main.py
+++ models/coupled_well_reservoir_aquifer_2_perfs/main.py
@@ -12,7 +12,7 @@ redirect_darts_output('run_log.log')
 coupled_model = Model()
 coupled_model.init()
 
-if True:
+if 1:
     output_props = coupled_model.physics.property_operators[0].props_name
     coupled_model.output_to_vtk(ith_step=0, output_properties=output_props)   # saves initial conditions
 
@@ -23,23 +23,6 @@ if True:
                   1/24/6 - 1/24/12,   # 10 minute
                   1/24/3 - 1/24/6,   # 20 minute
                   1/24/2 - 1/24/3,   # 30 minute
-                  1.0001/24/6*5 - 1/24/3,   # 50 minute
-                  1/24 - 1.0001/24/6*5,   # 1 hour
-                  2.0001/24 - 1/24,   # 2 hour
-                  3/24 - 2.0001/24,   # 3 hour
-                  4.99/24 - 3/24,   # 5 hour
-                  9.98/24 - 4.99/24,   # 10 hour
-                  1 - 9.98/24,   # 1 day
-                  2 - 1,   # 2 day
-                  3 - 2,   # 3 day
-                  5 - 3,   # 5 day
-                  10 - 5,   # 10 day
-                  19.99 - 10,   # 20 day
-                  30.002 - 19.99,   # 30 day
-                  49.99 - 30.002,   # 50 day
-                  100.02 - 49.99,   # 100 day
-                  200.001 - 100.02,   # 200 day
-                  365 - 200.001,   # 365 day
                   ]
 
     for i, dt in enumerate(time_steps):
