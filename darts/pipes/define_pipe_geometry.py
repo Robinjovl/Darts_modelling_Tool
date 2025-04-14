b6dabc1074bbea3df1b0115077b59bf5f2922fd3
Sajjad Moslehi <s.moslehi@tudelft.nl>
2025-04-14 19:34:13 +0200

    Correct PETREL_PipeGeometry

--- darts/pipes/define_pipe_geometry.py
+++ darts/pipes/define_pipe_geometry.py
@@ -130,8 +130,7 @@ class PETREL_PipeGeometry(PipeGeometry):
 
             inclination_angles.append(theta_deg)
 
-            inclination_angles = np.array(inclination_angles)
-            conn_inclination_angles = (inclination_angles[:-1] + inclination_angles[1:]) / 2
+            conn_inclination_angles = (np.array(inclination_angles[:-1]) + np.array(inclination_angles[1:])) / 2
 
         # Create the result DataFrame. This is not used in any part of the code.
         self.segments_info = pd.DataFrame({
