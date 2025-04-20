c8f2f2e9ef0227b91309e147adcecda985d8ecf6
Sajjad Moslehi <s.moslehi@tudelft.nl>
2025-04-20 15:16:52 +0200

    Increase max time step size over time

--- models/coupled_well_reservoir_aquifer_2_perfs/main.py
+++ models/coupled_well_reservoir_aquifer_2_perfs/main.py
@@ -26,6 +26,13 @@ if 1:
                   ]
 
     for i, dt in enumerate(time_steps):
+        if i == 1:
+            coupled_model.params.max_ts = 2 / (24 * 60 * 60)
+        elif i == 2:
+            coupled_model.params.max_ts = 5 / (24 * 60 * 60)
+        elif i == 5:
+            coupled_model.params.max_ts = 10 / (24 * 60 * 60)
+
         coupled_model.run(dt)
         coupled_model.output_to_vtk(ith_step=i+1, output_properties=output_props)
 
--- models/coupled_well_reservoir_depleted_reservoir_SPE_paper_thermal/main.py
+++ models/coupled_well_reservoir_depleted_reservoir_SPE_paper_thermal/main.py
@@ -49,6 +49,20 @@ if 1:
                   ]
 
     for i, dt in enumerate(time_steps):
+        if i == 1:
+            coupled_model.params.max_ts = 2 / (24 * 60 * 60)
+        elif i == 2:
+            coupled_model.params.max_ts = 5 / (24 * 60 * 60)
+        elif i == 5:
+            coupled_model.params.max_ts = 10 / (24 * 60 * 60)
+        elif i == 7:
+            coupled_model.params.max_ts = 60 / (24 * 60 * 60)
+        elif i == 9:
+            coupled_model.params.max_ts = 600 / (24 * 60 * 60)
+        elif i == 11:
+            coupled_model.params.max_ts = 6000 / (24 * 60 * 60)
+        elif i == 16:
+            coupled_model.params.max_ts = 60000 / (24 * 60 * 60)
         coupled_model.run(dt)
         coupled_model.output_to_vtk(ith_step=i+1, output_properties=output_props)
 
