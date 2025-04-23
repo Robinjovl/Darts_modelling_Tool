263d39d487758c7a5a1b561e7cf659812d1ffd47
Ilshat Saifullin <I.S.Saifullin@tudelft.nl>
2025-04-23 17:24:52 +0200

    moved dict_hash to a separate file utils.py

--- models/displaced_fault_reactivation/reservoir.py
+++ models/displaced_fault_reactivation/reservoir.py
@@ -19,6 +19,7 @@ from darts.reservoirs.mesh.transcalc import TransCalculations as TC
 rcParams["text.usetex"]=False
 # rcParams["font.sans-serif"] = ["Liberation Sans"]
 # rcParams["font.serif"] = ["Liberation Serif"]
+from utils import dict_hash, hash_array
 
 class UnstructReservoir:
     def __init__(self, timer, fluid_density, rock_density, mesh_file, cache_discretizer: bool = True):
@@ -1227,44 +1228,3 @@ class UnstructReservoir:
             self.fig.tight_layout()
             self.fig.savefig(output_directory + '/fig_' + str(ith_step) + '.png')
             plt.close(self.fig)
-
-from typing import Dict, Any
-import hashlib
-import json
-from hashlib import sha1
-
-def dict_hash(dict: Dict[str, Any]) -> str:
-    # returns a MD5 hash of a dictionary
-    dhash = hashlib.md5()
-    # sort arguments since {'a': 1, 'b': 2} is the same as {'b': 2, 'a': 1}
-
-    dict_for_hash = {}
-    for k in dict.keys():
-        if k in ['self.contacts']:
-            continue
-        elif k in ['self.pm']:
-            for kk in ['cell_m', 'cell_p', 'stencil', 'offset', 'tran', 'rhs', 'tran_biot', 'rhs_biot']:
-                dict_for_hash[k + '.' + kk] = hash_array(getattr(dict[k], kk))
-        elif k in ['self.unstr_discr']:
-            for kk in ['mat_cells_tot', 'frac_cells_tot']:
-                dict_for_hash[k + '.' + kk] = str(getattr(dict[k], kk))
-        else:
-            dict_for_hash[k] = hash_array(dict[k])
-
-    print('dict_for_hash', dict_for_hash)
-    encoded = json.dumps(dict_for_hash, sort_keys=True).encode()
-    dhash.update(encoded)
-    return dhash.hexdigest()
-
-def hash_array(a):
-    iv = type(index_vector())
-    vv = type(value_vector())
-    if type(a) in [iv, vv]:  # value/index_vector to numpy array
-        tmp = np.array(a)
-    else:
-        tmp = a
-    if isinstance(tmp, np.ndarray):
-        b = sha1(tmp).hexdigest()  # str with a hash of an array
-    else:
-        b = a  # or the same object
-    return b
