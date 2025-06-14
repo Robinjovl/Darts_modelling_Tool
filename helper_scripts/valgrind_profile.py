e9180cb827dff5d250e1a417f73ef5137ee08e37
Ilshat Saifullin <I.S.Saifullin@tudelft.nl>
2025-06-14 23:25:17 +0200

    added ccs and spe11b to be checked by valgrind

--- helper_scripts/valgrind_profile.py
+++ helper_scripts/valgrind_profile.py
@@ -16,7 +16,9 @@ valgrind_models = [ '2ph_comp', '2ph_comp_solid', '2ph_do',
                     'GeoRising',
                     'CoaxWell',
                     'phreeqc_dissolution',
-                    '2ph_do_thermal_mpfa'
+                    '2ph_do_thermal_mpfa',
+                    'CCS',
+                    'SPE11b'
                 ]       
 
 # if the user passed extra models, append them (comma-separated):
