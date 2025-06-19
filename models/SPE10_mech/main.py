e69f42d09e8d8c128f3502cf8d8d8e2ae88dc68f
Ilshat Saifullin <I.S.Saifullin@tudelft.nl>
2025-06-19 22:25:05 +0200

    also set to run that case in main

--- models/SPE10_mech/main.py
+++ models/SPE10_mech/main.py
@@ -264,4 +264,5 @@ if __name__ == '__main__':
 
 
     #run(model_folder='6_6_5', physics_type='single_phase_thermal', generate_mesh=True, decouple_geomech=True)
-    run(model_folder='16_16_15', physics_type='single_phase_thermal', generate_mesh=True, decouple_geomech=True)
+    #run(model_folder='16_16_15', physics_type='single_phase_thermal', generate_mesh=True, decouple_geomech=True)
+    run(model_folder='28_28_37', physics_type='single_phase_thermal', generate_mesh=True, decouple_geomech=True)
