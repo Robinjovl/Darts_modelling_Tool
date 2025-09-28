8a87a4726bccd2817b70806df037e0ad021c5601
Ilshat Saifullin <13548357-i-l-s-h-a-t@users.noreply.gitlab.com>
2025-09-28 09:51:45 +0000

    Unstr Discr fix ordering for 3D case with fractures

--- darts/reservoirs/mesh/unstruct_discretizer.py
+++ darts/reservoirs/mesh/unstruct_discretizer.py
@@ -299,18 +299,27 @@ class UnstructDiscretizer:
             bound_count = 0
             frac_bound_count = 0
             output_count = 0
-            self.geom_order = [
-                geom[0]
-                for geom in sorted(
-                    list(self.mesh_data.cell_data_dict['gmsh:physical'].items()),
-                    key=lambda x: -x[1][0],
-                )
-            ]
 
-            for geometry, tags in sorted(
-                list(self.mesh_data.cell_data_dict['gmsh:physical'].items()),
-                key=lambda x: -x[1][0],
-            ):
+            # the loop below has conditions such that the elements are looped over in the following order: 'matrix', 'fracture', 'boundary', 'fracture_boundary', 'output'
+            # however, the outer loop is over geometry types, so need to find the geometries in the right order first
+            # in 3D case, 'fracture', 'boundary' both might have the same geometry type (e.g. 'triangle') and there is no issue as there is an ordering in the inner loops below
+            self.geom_order = []
+            for elem_type in [
+                'matrix',
+                'fracture',
+                'boundary',
+                'fracture_boundary',
+                'output',
+            ]:
+                if elem_type in self.physical_tags.keys():
+                    for k, t in self.mesh_data.cell_data_dict['gmsh:physical'].items():
+                        for mat_tag in self.physical_tags[elem_type]:
+                            if mat_tag in t:
+                                if k not in self.geom_order:
+                                    self.geom_order.append(k)
+
+            for geometry in self.geom_order:
+                tags = self.mesh_data.cell_data_dict['gmsh:physical'][geometry]
                 # Main loop over different existing geometries
                 for ith_cell, nodes_to_cell in enumerate(
                     self.mesh_data.cells_dict[geometry]
