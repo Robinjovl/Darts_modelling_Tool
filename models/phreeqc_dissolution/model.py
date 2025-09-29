69555583dddd4e5070f774c70c95fa7608708915
smoslehi <s.moslehi@tudelft.nl>
2025-09-29 18:21:08 +0200

    Fix part of the problem with API of phreeqc_dissolution CI test

--- models/phreeqc_dissolution/model.py
+++ models/phreeqc_dissolution/model.py
@@ -495,7 +495,7 @@ class Model(CICDModel):
         #                                    verbose=True, well_radius=r_w, well_index=well_index,
         #                                    well_indexD=well_index)
 
-        self.reservoir.add_well("P1", well_type, wellbore_diameter=d_w)
+        self.reservoir.add_well("P1", well_type, well_ID=d_w)
         if self.domain == '3D':
             for idx in self.prd_cells:
                 self.reservoir.add_perforation(well_name='P1', res_cell_idx=idx, multi_segment=False,
