fixing signs for stress (positive sign for compressive stresses), fix thm unit after prev. changes, change a wayof making positive for compressive stresses when reading thm stresses (should fix delta_eff_stress)

models/SPE10_mech/main_proxy.py
@@ -132,7 +132,7 @@ def run_geomech_proxy(case, physics_type='single_phase', wells_type=None, timest
         cell = find_cell_by_point(point)
         if verbose:
             print('get_thm_solution', 'closest cell is', centroids[cell, :], 'point', point)
-        return delta_Sxx_last[cell]*0.1,  delta_Syy_last[cell]*0.1,  delta_Szz_last[cell]*0.1
+        return delta_Sxx_last[cell],  delta_Syy_last[cell],  delta_Szz_last[cell]
 
     def get_thm_stress_by_deriv(point): 
         # compute strain and stress in python from THM displacements (ux_last, etc)
@@ -219,11 +219,11 @@ def run_geomech_proxy(case, physics_type='single_phase', wells_type=None, timest
         
 
         # volumetric_strain=div(displ)
-        volumetric_strain = dux_dx + duy_dy + duz_dz
+        volumetric_strain = -(dux_dx + duy_dy + duz_dz)
 
         n_points = 1
         kronecker = np.array(n_points * [1, 1, 1, 0, 0, 0]).reshape(n_points, 6).transpose()
-        stress = g.young * (strain + g.poisson / (1 - 2 * g.poisson) *
+        stress = g.young * (-strain + g.poisson / (1 - 2 * g.poisson) *
                                volumetric_strain * kronecker) / (1 + g.poisson)
 
         return strain[0], strain[1], strain[2], stress[0], stress[1], stress[2]  # Sxx, Syy, Szz
@@ -384,6 +384,7 @@ def run_geomech_proxy(case, physics_type='single_phase', wells_type=None, timest
         modes = ['displ_z', 'displ_y', 'displ_x']
         modes += ['strain_z', 'strain_y', 'strain_x']
         modes += ['delta_stress_z', 'delta_stress_y', 'delta_stress_x']
+        #modes = ['strain_z']
 
         # compare U-Z at a line along z-axis
         z_min = 0.

models/SPE10_mech/reservoir.py
@@ -256,12 +256,12 @@ class UnstructReservoirCustom(UnstructReservoirMech):
 
         # Stresses and velocities
         engine.eval_stresses_and_velocities()
-        total_stresses = np.array(engine.total_stresses, copy=False)
+        total_stresses = -np.array(engine.total_stresses, copy=False)# make positive for compressive stresses
 
         if not hasattr(self, 'displs_initial'):
             self.displs_initial = dict()
         if not hasattr(self, 'tot_stress_initial'):
-            self.tot_stress_initial = total_stresses.copy()
+            self.tot_stress_initial = total_stresses.copy() 
 
         # Matrix
         cells = []
@@ -292,12 +292,12 @@ class UnstructReservoirCustom(UnstructReservoirMech):
                 if 'tot_stress' not in cell_data: cell_data['tot_stress'] = []
                 cell_data['tot_stress'].append(np.zeros((self.n_matrix, 6), dtype=np.float64))
                 for j in range(6):
-                    cell_data['tot_stress'][-1][:, j] = -(total_stresses[j::6])
+                    cell_data['tot_stress'][-1][:, j] = total_stresses[j::6]
 
                 if 'delta_tot_stress' not in cell_data: cell_data['delta_tot_stress'] = []
                 cell_data['delta_tot_stress'].append(np.zeros((self.n_matrix, 6), dtype=np.float64))
                 for j in range(6):
-                    cell_data['delta_tot_stress'][-1][:, j] = -(total_stresses[j::6]) - np.fabs(self.tot_stress_initial[j::6])
+                    cell_data['delta_tot_stress'][-1][:, j] = total_stresses[j::6] - self.tot_stress_initial[j::6]
 
                 delta_pressure = pressure - self.pressure_initial
                 if 'delta_pressure' not in cell_data: cell_data['delta_pressure'] = []
@@ -307,7 +307,7 @@ class UnstructReservoirCustom(UnstructReservoirMech):
                 if 'delta_eff_stress' not in cell_data: cell_data['delta_eff_stress'] = []
                 cell_data['delta_eff_stress'].append(np.zeros((self.n_matrix, 6), dtype=np.float64))
                 for j in range(6):
-                    cell_data['delta_eff_stress'][-1][:, j] = -(total_stresses[j::6] - self.tot_stress_initial[j::6] + delta_pressure)
+                    cell_data['delta_eff_stress'][-1][:, j] = cell_data['delta_tot_stress'][-1][:, j] - delta_pressure
 
                 if hasattr(self, 'temperature_initial'): # if thermal simulation
                     if 'delta_temperature' not in cell_data: cell_data['delta_temperature'] = []