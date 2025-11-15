6e45f106c2ece2812fc2d9e94ecfe72edf5b2856
smoslehi <s.moslehi@tudelft.nl>
2025-11-15 13:55:14 +0100

    Use upwind scheme for potential energy flux

--- engines/src/conn_mesh.cpp
+++ engines/src/conn_mesh.cpp
@@ -36,7 +36,6 @@ conn_mesh::init(std::vector<index_t>& block_m, std::vector<index_t>& block_p, st
   depth.assign(n_res_blocks, 0);
   heat_capacity.assign(n_res_blocks, 0);
   rock_cond.assign(n_res_blocks, 0);
-  cell_thickness.assign(n_res_blocks, 0);
 
   // kinetic property
   kin_factor.assign(n_res_blocks, 1);  // if I want backwards compatibility with older version of python files I assume it needs to be filled with a 1 here (in case people don't actually use this factor!)
@@ -1748,28 +1747,6 @@ int conn_mesh::init_spe(value_t grav_acceleration_for_spe)
 		cell_spe[i] = - (depth[i] - depth[0]) * grav_acceleration_for_spe * 1e-3;
 	}
 
-	// Calculate and store specific potential energy (spe) at connections
-	conn_spe.assign(n_conns, 0);
-
-	value_t half_dz_m;
-	value_t half_dz_p;
-	value_t z_m;
-	value_t z_p;
-	value_t z_conn;
-	for (index_t j = 0; j < n_conns; ++j)
-	{
-		half_dz_m = cell_thickness[block_m[j]] / 2;
-		half_dz_p = cell_thickness[block_p[j]] / 2;
-		z_m = depth[block_m[j]];
-		z_p = depth[block_p[j]];
-		z_conn = (half_dz_m * z_p + half_dz_p * z_m) / (half_dz_m + half_dz_p);
-
-		// It is multiplied by -1 because for spe height needs to be used instead of depth
-		// It is multiplied by 1e-3 to convert Joule to kilo Joule
-		// Use depth[0] as the reference depth
-		conn_spe[j] = - (z_conn - depth[0]) * grav_acceleration_for_spe * 1e-3;
-	}
-
 	return 0;
 }
 
@@ -1899,7 +1876,6 @@ int conn_mesh::add_wells(std::vector<ms_well *> &wells)
   initial_state.resize(n_blocks * n_vars);
   op_num.resize(n_blocks);
   depth.resize(n_blocks + n_bounds);
-  cell_thickness.resize(n_blocks);
 
   heat_capacity.resize(n_blocks);
   rock_cond.resize(n_blocks + n_bounds);
@@ -1925,7 +1901,6 @@ int conn_mesh::add_wells(std::vector<ms_well *> &wells)
         rock_cond[w_i] = rock_cond[r_i];
         // depth of well segments
         depth[wells[iw]->well_head_idx + p] = wells[iw]->well_body_depth + (p - 1) * wells[iw]->segment_depth_increment;
-		cell_thickness[wells[iw]->well_head_idx + p] = cell_thickness[r_i];
       }
     }
   }
