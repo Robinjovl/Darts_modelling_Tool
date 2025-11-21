5d98f8aacfd2b6d306a46dfe66c78988ebac80fc
smoslehi <s.moslehi@tudelft.nl>
2025-11-21 14:00:10 +0100

    Add a CI test for coupled well-reservoir modeling for liquid CO2 injection in a depleted reservoir

--- models/run_test_suite2.py
+++ models/run_test_suite2.py
@@ -11,16 +11,18 @@ def run_testing(platform, overwrite, iter_solvers, test_all_models):
 
     # set model list to run
 
-    accepted_dirs = ['2ph_comp', '2ph_comp_solid', '2ph_do',
-                     '2ph_geothermal', '2ph_geothermal_mass_flux',
-                     '3ph_comp_w', '3ph_do', '3ph_bo',
-                     'Uniform_Brugge',
-                     'Chem_benchmark_new',
-                     #'CO2_foam_CCS',
-                     'GeoRising',
-                     'CoaxWell',
-                     'phreeqc_dissolution',
-                     'effect_of_potential_energy',
+    accepted_dirs = [
+        'coupled_well_reservoir_model',
+        '2ph_comp', '2ph_comp_solid', '2ph_do',
+        '2ph_geothermal', '2ph_geothermal_mass_flux',
+        '3ph_comp_w', '3ph_do', '3ph_bo',
+        'Uniform_Brugge',
+        'Chem_benchmark_new',
+        #'CO2_foam_CCS',
+        'GeoRising',
+        'CoaxWell',
+        'phreeqc_dissolution',
+        'effect_of_potential_energy',
                      ]
 
 
