0d6424c757120efe56f9784b7ef32128c453d345
smoslehi <s.moslehi@tudelft.nl>
2025-12-01 21:42:47 +0100

    Revert my change related to solid because this change was not necessary from the beginning and I don't work with solid in well at the moment

--- darts/physics/super/operator_evaluator.py
+++ darts/physics/super/operator_evaluator.py
@@ -266,9 +266,7 @@ class WellOperators(OperatorsSuper):
             self.property.sat[: self.np_fl] * self.property.dens_m[: self.np_fl]
         )
         zc = np.append(state_np[1 : self.nc], 1 - np.sum(state_np[1 : self.nc]))
-        # self.phi_f = 1.
-        self.phi_s = np.sum(zc[self.nc_fl :])
-        self.phi_f = 1.0 - self.phi_s
+        self.phi_f = 1.0
 
         """ CONSTRUCT OPERATORS HERE """
 
