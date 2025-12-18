9e1f3308ad609e57609eae5a8796b0874408fb85
Ilshat Saifullin <I.S.Saifullin@tudelft.nl>
2025-12-18 14:22:22 +0100

    added strain rate rsv part; skip some plots (1d and some 2d plots), plot at X=0 (close to fault where the meh is refined), changed cmap

--- models/displaced_fault_reactivation/plot.py
+++ models/displaced_fault_reactivation/plot.py
@@ -37,8 +37,9 @@ def plot_contour(fname_prefix, array_dict, output_folder, points_x=None, points_
             if False:
                 cs = plt.contourf(points_x, points_y, arr_layer, levels=10)
             else:
+                point_size = 0.0001
                 x1, y1 = np.meshgrid(points_x, points_y)
-                cs = plt.scatter(x1, y1, c=arr_layer, s=0.001) # point size i s
+                cs = plt.scatter(x1, y1, c=arr_layer, s=point_size, cmap='plasma')
         plt.colorbar(cs)
         #plt.gca().set_aspect('equal')
         #plt.tight_layout()
@@ -48,24 +49,25 @@ def plot_contour(fname_prefix, array_dict, output_folder, points_x=None, points_
         plt.savefig(os.path.join(output_folder, fname_prefix + arr_name + '.png'))
         plt.close()
 
-def plot_strain(tstep_plot_name, tstep_plot):
+def plot_strain(tstep_plot_name, tstep_plot, plot_1d=False):
     fname_prefix = tstep_plot_name + '_'
 
-    import pyvista as pv
-    pvd_filename = os.path.join(vtk_directory, 'solution.pvd')
-    reader = pv.get_reader(pvd_filename)
-    t_steps = np.asarray(reader.time_values)[tstep_plot]
-    print("Timesteps for plotting:", t_steps[:5], '...', t_steps[-5:])
+    if True: # set to False if run for unfinished simulation, where there is no .pvd (it stored in the end of simulation)
+        import pyvista as pv
+        pvd_filename = os.path.join(vtk_directory, 'solution.pvd')
+        reader = pv.get_reader(pvd_filename)
+        t_steps = np.asarray(reader.time_values)[tstep_plot]
+        print("Timesteps for plotting:", t_steps[:5], '...', t_steps[-5:])
 
     vtk_files = []
     for ti in tstep_plot:
         vtk_files.append(os.path.join(vtk_directory, 'solution' + str(ti) + '.vtu'))
 
     # Define points along the well and at the surface
-    y_range_well = np.arange(2000., -2000., -1.)
+    y_range_well = np.arange(2000., -2000., -0.2)
     n_points_well = y_range_well.size
     points_well = np.zeros((n_points_well, 3))
-    points_well[:,0] = 500.
+    points_well[:,0] = 0.
     points_well[:,1] = y_range_well
     points_well[:,2] = 250.
 
@@ -127,63 +129,65 @@ def plot_strain(tstep_plot_name, tstep_plot):
 
         v_y_well_list.append(v_y_well)
 
-    # Plot strain_yy along the well
-    for k, strain_yy_well in enumerate(strain_yy_well_list):
-        plt.plot(y_range_well, strain_yy_well, label='tstep_'+str(tstep_plot[k]))#, marker='.')
-    plt.xlabel('Y-coordinate, m.')
-    plt.title('Strain YY change along the well')
-    plt.ylabel('Strain YY change')
-    plt.legend()
-    plt.grid()
-    plt.savefig(os.path.join(output_directory, fname_prefix + 'strain_yy_well.png'))
-    #plt.show()
-    plt.close()
+    if plot_1d:
+        # Plot strain_yy along the well
+        for k, strain_yy_well in enumerate(strain_yy_well_list):
+            plt.plot(y_range_well, strain_yy_well, label='tstep_'+str(tstep_plot[k]))#, marker='.')
+        plt.xlabel('Y-coordinate, m.')
+        plt.title('Strain YY change along the well')
+        plt.ylabel('Strain YY change')
+        plt.legend()
+        plt.grid()
+        plt.savefig(os.path.join(output_directory, fname_prefix + 'strain_yy_well.png'))
+        #plt.show()
+        plt.close()
 
-    # Plot strain_yy along the well
-    for k, strain_rate_yy_well in enumerate(strain_rate_yy_well_list):
-        plt.plot(y_range_well, strain_rate_yy_well, label='tstep_'+str(tstep_plot[k]))#, marker='.')
-    plt.xlabel('Y-coordinate, m.')
-    plt.title('Strain rate YY change along the well')
-    plt.ylabel('Strain rate YY change')
-    plt.legend()
-    plt.grid()
-    plt.savefig(os.path.join(output_directory, fname_prefix + 'strain_rate_yy_well.png'))
-    #plt.show()
-    plt.close()
+        # Plot strain_yy along the well
+        for k, strain_rate_yy_well in enumerate(strain_rate_yy_well_list):
+            plt.plot(y_range_well, strain_rate_yy_well, label='tstep_'+str(tstep_plot[k]))#, marker='.')
+        plt.xlabel('Y-coordinate, m.')
+        plt.title('Strain rate YY change along the well')
+        plt.ylabel('Strain rate YY change')
+        plt.legend()
+        plt.grid()
+        plt.savefig(os.path.join(output_directory, fname_prefix + 'strain_rate_yy_well.png'))
+        #plt.show()
+        plt.close()
 
-    # Plot strain_xx along the surface
-    for k, strain_xx_surface in enumerate(strain_xx_surface_list):
-        plt.plot(x_range_surface, strain_xx_surface, label='tstep_'+str(tstep_plot[k]))#, marker='.')
-    plt.xlabel('X-coordinate, m.')
-    plt.title('Strain XX change along the line at the surface')
-    plt.ylabel('Strain XX change')
-    plt.legend()
-    plt.grid()
-    plt.savefig(os.path.join(output_directory,  fname_prefix + 'strain_xx_surface.png'))
-    #plt.show()
-    plt.close()
+        # Plot strain_xx along the surface
+        for k, strain_xx_surface in enumerate(strain_xx_surface_list):
+            plt.plot(x_range_surface, strain_xx_surface, label='tstep_'+str(tstep_plot[k]))#, marker='.')
+        plt.xlabel('X-coordinate, m.')
+        plt.title('Strain XX change along the line at the surface')
+        plt.ylabel('Strain XX change')
+        plt.legend()
+        plt.grid()
+        plt.savefig(os.path.join(output_directory,  fname_prefix + 'strain_xx_surface.png'))
+        #plt.show()
+        plt.close()
 
-    # Plot strain_yy along the well
-    for k, v_y_well in enumerate(v_y_well_list):
-        plt.plot(y_range_well, v_y_well, label='tstep_'+str(tstep_plot[k]))#, marker='.')
-    plt.xlabel('Y-coordinate, m.')
-    plt.title('Velocity Y along the well')
-    plt.ylabel('Velocity Y')
-    plt.legend()
-    plt.grid()
-    plt.savefig(os.path.join(output_directory,  fname_prefix + 'velocity_y_well.png'))
-    #plt.show()
-    plt.close()
+        # Plot strain_yy along the well
+        for k, v_y_well in enumerate(v_y_well_list):
+            plt.plot(y_range_well, v_y_well, label='tstep_'+str(tstep_plot[k]))#, marker='.')
+        plt.xlabel('Y-coordinate, m.')
+        plt.title('Velocity Y along the well')
+        plt.ylabel('Velocity Y')
+        plt.legend()
+        plt.grid()
+        plt.savefig(os.path.join(output_directory,  fname_prefix + 'velocity_y_well.png'))
+        #plt.show()
+        plt.close()
 
     ############ contour
-    # surface
-    n_timesteps = len(strain_xx_surface_list)
-    n_points = strain_xx_surface_list[0].size
-    strain_2d = np.zeros((n_points, n_timesteps))
-    for k in range(n_timesteps):
-        strain_2d[:, k] = strain_xx_surface_list[k]
-    array_dict = {'Strain_XX_change_surface_contour': strain_2d}
-    plot_contour(fname_prefix, array_dict, points_x=tstep_plot, points_y=x_range_surface, output_folder=output_directory)
+    if False:
+        # surface
+        n_timesteps = len(strain_xx_surface_list)
+        n_points = strain_xx_surface_list[0].size
+        strain_2d = np.zeros((n_points, n_timesteps))
+        for k in range(n_timesteps):
+            strain_2d[:, k] = strain_xx_surface_list[k]
+        array_dict = {'Strain_XX_change_surface_contour': strain_2d}
+        plot_contour(fname_prefix, array_dict, points_x=tstep_plot, points_y=x_range_surface, output_folder=output_directory)
     # well - strain change
     n_timesteps = len(strain_yy_well_list)
     n_points = strain_yy_well_list[0].size
@@ -204,16 +208,19 @@ def plot_strain(tstep_plot_name, tstep_plot):
         strain_2d[:, k] = strain_rate_yy_well_list[k]
     array_dict = {'Strain_rate_YY_change_well_contour': strain_2d}
     plot_contour(fname_prefix, array_dict, points_x=tstep_plot, points_y=y_range_well, output_folder=output_directory)
+    # well rsv part - strain rate change
+    array_dict = {'Strain_rate_YY_change_well_contour_rsv': strain_2d[y_range_well_rsv, :]}
+    plot_contour(fname_prefix, array_dict, points_x=tstep_plot, points_y=y_range_well[y_range_well_rsv], output_folder=output_directory)
 
-
-    # well
-    n_timesteps = len(v_y_well_list)
-    n_points = v_y_well_list[0].size
-    strain_2d = np.zeros((n_points, n_timesteps))
-    for k in range(n_timesteps):
-        strain_2d[:, k] = v_y_well_list[k]
-    array_dict = {'Velocity_Y_well_contour': strain_2d}
-    plot_contour(fname_prefix, array_dict, points_x=tstep_plot, points_y=y_range_well, output_folder=output_directory)
+    # well - velocity
+    if False:
+        n_timesteps = len(v_y_well_list)
+        n_points = v_y_well_list[0].size
+        strain_2d = np.zeros((n_points, n_timesteps))
+        for k in range(n_timesteps):
+            strain_2d[:, k] = v_y_well_list[k]
+        array_dict = {'Velocity_Y_well_contour': strain_2d}
+        plot_contour(fname_prefix, array_dict, points_x=tstep_plot, points_y=y_range_well, output_folder=output_directory)
 
     ############# fault
     if False:
