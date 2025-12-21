fed3da6254f6724dad932c023bc37519322a5d84
smoslehi <s.moslehi@tudelft.nl>
2025-12-21 14:01:20 +0100

    Update API of CI test related to change in previous commit

--- models/coupled_well_reservoir_model/main.py
+++ models/coupled_well_reservoir_model/main.py
@@ -14,9 +14,9 @@ from model import Model
 from darts.engines import redirect_darts_output
 from darts.tools.hdf5_tools import load_hdf5_to_dict
 from darts.pipes.save_results import save_segments_primary_vars_and_phase_props
-from darts.pipes.plot_heat_map_pcolormesh import plot_heat_map_pcolormesh
-from darts.pipes.plot_line_graphs import plot_line_graphs
-from darts.pipes.plot_heat_map_contourf import plot_heat_map_contourf
+from darts.pipes.viz.plot_heat_map_pcolormesh import plot_heat_map_pcolormesh
+from darts.pipes.viz.plot_line_graphs import plot_line_graphs
+from darts.pipes.viz.plot_heat_map_contourf import plot_heat_map_contourf
 
 redirect_darts_output('run_log.log')
 coupled_model = Model()
