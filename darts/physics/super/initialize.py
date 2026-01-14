5eaa8113278a0c167465cd9cc5d0547511433aba
Michiel Wapperom <m.b.wapperom@tudelft.nl>
2026-01-14 16:37:14 +0100

    Keep only solve() and solve_region() methods

--- darts/physics/super/initialize.py
+++ darts/physics/super/initialize.py
@@ -152,116 +152,6 @@ class Initialize:
 
         return values, derivs
 
-    def solve(
-        self,
-        depth_bottom: float,
-        depth_top: float,
-        depth_known: float,
-        boundary_state: dict,
-        primary_specs: dict = None,
-        secondary_specs: dict = None,
-        nb: int = 100,
-        dTdh: float = 0.03,
-    ):
-        """
-        Solve for all depths
-
-        :param depth_bottom: Bottom depth
-        :param depth_top: Top depth
-        :param depth_known: Depth of known conditions
-        :param boundary_state: Known conditions, set of primary and secondary specifications
-        :type boundary_state: dict
-        :param primary_specs: Primary specifications
-        :type primary_specs: dict
-        :param secondary_specs: Secondary specifications
-        :type secondary_specs: dict
-        :param nb: Number of depth values
-        :param dTdh: Temperature gradient
-        """
-        # If only one block has been specified, return the boundary state
-        if nb == 1:
-            self.depths = np.array([depth_known])
-            return np.array(
-                [boundary_state[variable] for variable in self.var_idxs.keys()]
-            )
-
-        # Else, check input and create depths
-        assert depth_bottom >= depth_top, "Top depth is below bottom depth"
-        assert depth_top <= depth_known <= depth_bottom, (
-            "Known depth is not in range [bottom, top]"
-        )
-        self.depths = np.linspace(start=depth_top, stop=depth_bottom, num=nb)
-        bc_idx = (np.fabs(self.depths - depth_known)).argmin()
-        self.depths[bc_idx] = depth_known
-
-        # Set primary and secondary specifications
-        if primary_specs:
-            for spec, values in primary_specs.items():
-                self.primary_specs[spec] = (
-                    values
-                    if isinstance(values, list | np.ndarray)
-                    else np.ones(nb) * values
-                )
-                assert len(self.primary_specs[spec]) == nb, (
-                    "Length of " + spec + " not compatible"
-                )
-        if secondary_specs:
-            for spec, values in secondary_specs.items():
-                self.secondary_specs[spec] = (
-                    values
-                    if isinstance(values, list | np.ndarray)
-                    else np.ones(nb) * values
-                )
-                assert len(self.secondary_specs[spec]) == nb, (
-                    "Length of " + spec + " not compatible"
-                )
-        for i in range(nb):
-            if i != bc_idx:
-                assert (
-                    int(
-                        np.sum(
-                            [
-                                not np.isnan(np.float64(spec[i]))
-                                for spec in self.primary_specs.values()
-                            ]
-                        )
-                        + np.sum(
-                            [
-                                not np.isnan(np.float64(spec[i]))
-                                for spec in self.secondary_specs.values()
-                            ]
-                        )
-                    )
-                    == self.nv - 1 - self.thermal
-                ), (
-                    f"Not the right number of variables specified for well-defined system of equations in block {i}, need {self.nv - 1 - self.thermal}"
-                )
-
-        # Set state in known cell
-        X = np.zeros((nb, self.nv))
-        X[bc_idx] = np.array([boundary_state[v] for v in self.vars])
-        values0, _ = self.evaluate(X[bc_idx])
-
-        # Define thermal gradient
-        if self.thermal:
-            self.T = (
-                lambda i: values0[self.props_idxs['temperature']]
-                + (self.depths[i] - self.depths[bc_idx]) * dTdh
-            )
-
-        # Solve cells from specified cell upwards
-        for i in range(bc_idx, 0, -1):
-            self.solve_cell(X, cell_idx=i - 1, downward=False)
-
-        # Solve cells from specified cell downwards
-        for i in range(bc_idx, nb - 1):
-            self.solve_cell(X, cell_idx=i + 1, downward=True)
-
-        # Switch evaluate_PT boolean off to flash.evaluate() during simulation again
-        self.physics.property_containers[0].evaluate_PT_bool = False
-
-        return X.flatten()
-
     def init_depth_table(
         self,
         depth_bottom: float,
@@ -271,6 +161,17 @@ class Initialize:
         nb: int = 100,
         dTdh: float = 0.03,
     ):
+        """
+        Method to initialize depth table of nb grid blocks from min, max and known depth.
+
+        :param depth_bottom: Depth of bottom block
+        :param depth_top: Depth of top block
+        :param depth_known: Depth of known conditions
+        :param X0: Known conditions
+        :param nb: Number of grid blocks
+        :param dTdh: Temperature gradient [K/m]
+        :return:
+        """
         if nb == 1:
             self.depths = np.array([depth_known])
             bc_idx = 0
@@ -375,123 +276,134 @@ class Initialize:
         print("MAX ITER REACHED FOR INITIALIZATION", Xi)
         return Xi
 
-    def solve_cell(
-        self, X: np.ndarray, cell_idx: int, downward: bool = True, max_iter: int = 100
+    def solve(
+        self,
+        X: np.ndarray,
+        bc_idx: int,
+        specs: dict,
+        downward: bool = True,
+        region_idx: int = 0,
+        max_iter: int = 100,
     ):
         """
-        Solve for specific depth
+        Solve for all depths
 
-        :param X: State vector of all depths
+        :param X: State vector for all depths
         :type X: np.ndarray
-        :param cell_idx: Index of cell to solve
-        :param downward: Bool to indicate if known cell is above or below
+        :param bc_idx: Index of known cell
+        :type bc_idx: bool
+        :param specs: Set of specifications (primary and secondary variables)
+        :type specs: dict
+        :param region_idx: Index of property region for property container
+        :type region_idx: int
         :param max_iter: Maximum number of iterations
+        :type max_iter: int
         """
-        # Find neighbouring cell for which state is known
-        known_idx = cell_idx - 1 if downward else cell_idx + 1
+        # If only one block has been specified, return the boundary state
+        if len(self.depths) == 1:
+            return X
+
+        # Else, solve specification equations (primary and secondary variables)
+        temp_idx = self.props_idxs['temperature']
         rhoT_idx = self.props_idxs['rhoT']
         pot_idx = self.props_idxs['pot' + self.physics.phases[0]]
-        mob_idx = self.props_idxs['mob' + self.physics.phases[0]]
-        sat_idx = self.props_idxs['sat' + self.physics.phases[0]]
+        # mob_idx = self.props_idxs['mob' + self.physics.phases[0]]
+        # sat_idx = self.props_idxs['sat' + self.physics.phases[0]]
 
-        n_vars = self.nv - self.thermal
-        values0, _ = self.evaluate(X[known_idx])
-        gh0 = 9.81 * self.depths[known_idx] * 1e-5
-        gh1 = 9.81 * self.depths[cell_idx] * 1e-5
+        n_vars = self.nv  # - self.thermal
 
-        # Solve nonlinear unknowns
-        # Initialize using same composition, recalculate pressure and evaluate temperature gradient
-        X[cell_idx, 0] = values0[pot_idx] + values0[rhoT_idx] * (gh1 - gh0)
-        X[cell_idx, 1:] = X[known_idx, 1:]
-        if self.thermal:
-            X[cell_idx, -1] = self.T(cell_idx)
+        # Solve cells from specified cell upwards
+        cell_range = (
+            range(bc_idx, len(self.depths) - 1) if downward else range(bc_idx, 0, -1)
+        )
+        for i in cell_range:
+            # Find neighbouring cell for which state is known
+            cell_idx = i + 1 if downward else i - 1
+            known_idx = cell_idx - 1 if downward else cell_idx + 1
 
-        for _it in range(max_iter):
-            # nc variables for pressure and nc-1 compositions, temperature is calculated from gradient
-            res = np.zeros(n_vars)
-            Jac = np.zeros((n_vars, n_vars))
+            values0, _ = self.evaluate(X[known_idx])
+            gh0 = 9.81 * self.depths[known_idx] * 1e-5
+            gh1 = 9.81 * self.depths[cell_idx] * 1e-5
 
-            # Evaluate operators and derivatives at current state Xi
-            values1, derivs1 = self.evaluate(X[cell_idx])
+            # Solve nonlinear unknowns
+            # Initialize using same composition, recalculate pressure and evaluate temperature gradient
+            X[cell_idx, 0] = values0[pot_idx] + values0[rhoT_idx] * (gh1 - gh0)
+            X[cell_idx, 1:] = X[known_idx, 1:]
+            if self.thermal:
+                X[cell_idx, -1] = self.T(cell_idx)
 
-            # Zero mass flux equations for fluid phases
-            mgh = (values1[rhoT_idx] + values0[rhoT_idx]) * (gh1 - gh0) / 2
-            # j1, j2 = 0, 0
-            for j in range(self.physics.nph):
-                # Potential difference of phase j: P[1] - Pc[1] - (P[2] - Pc[2]) + mgh[av]
-                potential_diff = values0[pot_idx + j] - values1[pot_idx + j] + mgh
-                # potential = X[known_idx, 0] + mgh - values0[pc_idx + j] - X[cell_idx, 0] + values1[pc_idx + j]
+            for _it in range(max_iter):
+                # nc variables for pressure and nc-1 compositions, temperature is calculated from gradient
+                res = np.zeros(n_vars)
+                Jac = np.zeros((n_vars, n_vars))
 
-                if j == 0:
-                    if (
-                        values1[sat_idx + j]
-                        < self.secondary_specs['sr' + self.physics.phases[j]][cell_idx]
-                    ):
-                        # res_idx = j1 + j2 + n_vars
-                        res[j] = (
-                            values1[sat_idx + j]
-                            - self.secondary_specs['sr' + self.physics.phases[j]][
-                                cell_idx
-                            ]
-                        )
+                # Evaluate operators and derivatives at current state Xi
+                values1, derivs1 = self.evaluate(X[cell_idx])
 
-                        for k in range(n_vars):
-                            Jac[j, k] = derivs1[(sat_idx + j) * self.nv + k]
-                        # j2 += 1
-                    else:
-                        res[j] = values1[mob_idx + j] * potential_diff
-                        for k in range(self.nv):
-                            Jac[j, k] += values1[mob_idx + j] * (
-                                -derivs1[(pot_idx + j) * self.nv + k]
-                                + derivs1[rhoT_idx * self.nv + k] * (gh1 - gh0) / 2
-                            )
-                            Jac[j, k] += (
-                                derivs1[(mob_idx + j) * self.nv + k] * potential_diff
-                            )
-                else:
-                    res[j] = potential_diff
-                    for k in range(self.nv):
-                        Jac[j, k] += (
-                            -derivs1[(pot_idx + j) * self.nv + k]
-                            + derivs1[rhoT_idx * self.nv + k] * (gh1 - gh0) / 2
-                        )
-                    # res[j] = values1[mob_idx + j] * potential_diff
-                    # for k in range(self.nv):
-                    #     Jac[j, k] += values1[mob_idx + j] * (-derivs1[(pot_idx + j) * self.nv + k] + derivs1[rhoT_idx * self.nv + k] * (gh1 - gh0) / 2)
-                    #     Jac[j, k] += derivs1[(mob_idx + j) * self.nv + k] * potential_diff
+                # Zero mass flux equations for fluid phases
+                mgh = (values1[rhoT_idx] + values0[rhoT_idx]) * (gh1 - gh0) / 2
 
-            # Solve Newton step
-            dX = np.linalg.solve(Jac, res)
+                potential_diff = values0[pot_idx] - values1[pot_idx] + mgh
+                res[0] = potential_diff
+                for k in range(n_vars):
+                    Jac[0, k] += (
+                        -derivs1[pot_idx * self.nv + k]
+                        + derivs1[rhoT_idx * self.nv + k] * (gh1 - gh0) / 2
+                    )
 
-            # Calculate damping factor to remain within all positive mole fractions
-            betas_min = np.array([X[cell_idx, i] / dX[i] for i in range(1, self.nc)])
-            betas_max = np.array(
-                [-(1.0 - X[cell_idx, i]) / dX[i] for i in range(1, self.nc)]
-            )
-            beta = min(
-                1,
-                min(
-                    (
-                        np.amin(betas_min[betas_min > 0])
-                        if len(betas_min[betas_min > 0]) > 0
-                        else 1
+                # Specification equations of primary and secondary variables defined in self.props
+                res_idx = 1
+                for var, spec in specs.items():
+                    if not np.isnan(np.float64(spec)):
+                        prop_idx = self.props_idxs[var]
+                        res[res_idx] = values1[prop_idx] - spec
+
+                        for jj in range(self.nv):
+                            Jac[res_idx, jj] = derivs1[prop_idx * self.nv + jj]
+                        res_idx += 1
+
+                if self.thermal:
+                    res[-1] = values1[temp_idx] - self.T(cell_idx)
+                    for k in range(n_vars):
+                        Jac[-1, k] = derivs1[temp_idx * self.nv + k]
+
+                # Solve Newton step
+                dX = np.linalg.solve(Jac, res)
+
+                # Calculate damping factor to remain within all positive mole fractions
+                betas_min = np.array(
+                    [X[cell_idx, i] / dX[i] for i in range(1, self.nc)]
+                )
+                betas_max = np.array(
+                    [-(1.0 - X[cell_idx, i]) / dX[i] for i in range(1, self.nc)]
+                )
+                beta = min(
+                    1,
+                    min(
+                        (
+                            np.amin(betas_min[betas_min > 0])
+                            if len(betas_min[betas_min > 0]) > 0
+                            else 1
+                        ),
+                        (
+                            np.amin(betas_max[betas_max > 0])
+                            if len(betas_max[betas_max > 0]) > 0
+                            else 1
+                        ),
                     ),
-                    (
-                        np.amin(betas_max[betas_max > 0])
-                        if len(betas_max[betas_max > 0]) > 0
-                        else 1
-                    ),
-                ),
-            )
+                )
 
-            X[cell_idx, :n_vars] -= beta * dX
+                X[cell_idx, :n_vars] -= beta * dX
 
-            if np.linalg.norm(res) < 1e-10:
-                print(X[cell_idx])
-                print(values1)
-                return X
+                if np.linalg.norm(res) < 1e-10:
+                    break
 
-        print("MAX ITER REACHED FOR INITIALIZATION", X[cell_idx, :])
+            if _it > max_iter:
+                print("MAX ITER REACHED FOR INITIALIZATION", X[cell_idx, :])
+
+        # Switch evaluate_PT boolean off to flash.evaluate() during simulation again
+        self.physics.property_containers[0].evaluate_PT_bool = False
+
         return X
 
     def solve_region(
@@ -534,7 +446,7 @@ class Initialize:
         cell_range = (
             range(bc_idx, len(self.depths) - 1) if downward else range(bc_idx, 0, -1)
         )
-        for i in cell_range:
+        for i in range(cell_range):
             # Find neighbouring cell for which state is known
             cell_idx = i + 1 if downward else i - 1
             known_idx = cell_idx - 1 if downward else cell_idx + 1
@@ -664,4 +576,7 @@ class Initialize:
 
             if _it > max_iter:
                 print("MAX ITER REACHED FOR INITIALIZATION", X[cell_idx, :])
+
+        # Switch evaluate_PT boolean off to flash.evaluate() during simulation again
+        self.physics.property_containers[0].evaluate_PT_bool = False
         return X
