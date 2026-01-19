18cda5c159cef9281178ca3618a92df308bd8f1f
Michiel Wapperom <m.b.wapperom@tudelft.nl>
2026-01-19 17:54:46 +0100

    Use 5001 obl points to get closer to ref solution, original 5000 does not converge and 500 gives completely different results

--- models/3ph_bo/model.py
+++ models/3ph_bo/model.py
@@ -81,7 +81,7 @@ class Model(CICDModel):
         # example - how to change the properties
         # idata.fluid.density['water'] = DensityBasic(compr=1e-5, dens0=1014)
 
-        idata.obl.n_points = 500
+        idata.obl.n_points = 5001
         idata.obl.zero = 1e-12
         idata.obl.epsilon_z = 1e-13
         # idata.obl.epsilon_z = 0.
@@ -93,15 +93,5 @@ class Model(CICDModel):
         # idata.obl.min_z = 1e-13
         idata.obl.max_z = 1.
         # idata.obl.max_z = 1.-1e-13
-
-        if 0:
-            # working
-            idata.obl.epsilon_z = 0
-            idata.obl.min_z = 1e-13
-            idata.obl.max_z = 1.-1e-13
-        else:
-            idata.obl.epsilon_z = 1e-13
-            idata.obl.min_z = 0.
-            idata.obl.max_z = 1.
 
         return idata
