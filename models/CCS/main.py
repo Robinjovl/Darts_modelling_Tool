83fffd7b2496263c640f210352d7bfcf66248c30
Michiel Wapperom <m.b.wapperom@tudelft.nl>
2026-02-03 17:26:59 +0100

    Update CCS model with StructRadialReservoir

--- models/CCS/main.py
+++ models/CCS/main.py
@@ -8,70 +8,152 @@ import matplotlib.pyplot as plt
 redirect_darts_output('binary.log')
 
 
+class DataTS:
+    dt_min: float
+    omega: float
+    eta: float
+    dt_max: float
+    tol_res: float
+    tol_wel: float
+    tol_sta: float
+    max_it_nl: int
+    def __init__(self, nc):
+        self.eta = 1e20 * np.ones(nc)  # avoid limitation for changes
+
+        # default values
+        self.dt_min = 1e-2
+        self.omega = 1
+        self.dt_max = 365
+        self.tol_res = 1e-2
+        self.tol_wel_mult = 1
+        self.tol_sta = 1e-2
+        self.max_it_nl = 12
+
+
 filename = 'out'
 
 # define the model
 m = Model()
 
-# lower detfurth
-(nr, nz) = (1000, 24)
-if 1:
-    poro = np.ones((nr, nz)) * 0.001
-    perm = np.ones((nr, nz)) * 0.001
-    poro[:, 4:20] = 0.2
-    perm[:, 4:20] = 20
-    perm[:, 6:12] = 100
-else:
-    poro = np.ones((nr, nz)) * 0.075
-    perm = np.ones((nr, nz)) * 0.29
-    # upper detfurth
-    perm[:, :16] = 12.6
-    # hardegsen
-    poro[:, :10] = 0.09
-    perm[:, :10] = 24
-    # hardegsen high perm
-    poro[:, :8] = 0.2
-    perm[:, :8] = 550
-    # hardegsen
-    poro[:, :6] = 0.09
-    perm[:, :6] = 24
-    # caprock
-    poro[:, :4] = 0.01
-    perm[:, :4] = 0.01
+logspace = True
+if logspace:
+    dr = 1.
+    R1 = 1000.
+    nz = 24
 
-m.set_reservoir(nr=nr, dr=1., nz=nz, dz=5, poro=poro.flatten(order='F'), perm=perm.flatten(order='F'))
+    poro = np.ones(nz) * 0.001
+    perm = np.ones(nz) * 0.001
+    poro[4:20] = 0.2
+    perm[4:20] = 20
+    perm[6:12] = 100
+
+    m.set_reservoir(dr=dr, R1=R1, logspace=logspace, nz=nz, dz=5, poro=poro, perm=perm)
+
+else:
+    # lower detfurth
+    (nr, nz) = (10, 24)
+    dr = 1.
+    R1 = nr * dr
+
+    if 0:
+        poro = 0.2
+        perm = 100
+
+    elif 1:
+        poro = np.ones((nr, nz)) * 0.001
+        perm = np.ones((nr, nz)) * 0.001
+        poro[:, 4:20] = 0.2
+        perm[:, 4:20] = 20
+        perm[:, 6:12] = 100
+
+    else:
+        poro = np.ones((nr, nz)) * 0.075
+        perm = np.ones((nr, nz)) * 0.29
+        # upper detfurth
+        perm[:, :16] = 12.6
+        # hardegsen
+        poro[:, :10] = 0.09
+        perm[:, :10] = 24
+        # hardegsen high perm
+        poro[:, :8] = 0.2
+        perm[:, :8] = 550
+        # hardegsen
+        poro[:, :6] = 0.09
+        perm[:, :6] = 24
+        # caprock
+        poro[:, :4] = 0.01
+        perm[:, :4] = 0.01
+
+    m.set_reservoir(dr=dr, R1=R1, logspace=False, nz=nz, dz=5, poro=poro, perm=perm)
+
+# Specify initial and injection conditions
+m.p_init = 100.
+m.p_inj = m.p_init + 1.
+m.t_init = 350.
+m.t_inj = 300.
+m.swc = 0.25
+
+zero = 1e-12
+m.set_physics(zero, n_points=1001, temperature=None, ph=False, vl_phases=False)
+m.inj_stream = [0.001] if m.components[0] == "H2O" else [0.999]
+
+m.set_sim_params(first_ts=1e-7, mult_ts=2, max_ts=1., tol_newton=1e-6, tol_linear=1e-6, it_newton=8,
+                 it_linear=50,
+                 # newton_type=m.params.newton_global_chop,  # Type of newton method (related to chopping strategy?)
+                 # newton_params=value_vector([0.2]),  # Probably chop-criteria(?)
+                 )
+# m.params.nonlinear_norm_type = m.params.L1
+# m.params.linear_type = m.params.cpu_superlu
 
 # init the model
-m.ms_well_flag = True
 m.init()
 # set the output
-m.set_output(verbose = True)
+m.set_output()
 
 """ DEFINE OUTPUT """
-props = ['satV', 'rhoV']
+props = ['satV', 'rho_g']
 output_props = ['pressure'] + props if not m.physics.thermal else ['pressure', 'temperature'] + props
 # output_props += ['y' + comp for comp in m.components[2:]]
-t_max = m.t_inj if not m.physics.thermal else max(m.reservoir.mesh.initial_state[m.physics.n_vars-1::m.physics.n_vars])
+lims = {'pressure': [m.p_init, m.p_init+5], 'temperature': [m.t_inj - 30., m.t_init + 10.], 'satV': [1-m.swc, 1.]}
+lims.update({'satLCO2': [0., 1.]})
+# lims = None
 aspect = 'equal'  # 'equal', 'auto' or float
 cmap = 'RdBu_r'
 logx = True
 
 timestep, property_array = m.output.output_properties(output_properties=output_props, timestep=-1)
 
-m.reservoir.output_to_plt(data=property_array, output_props=output_props, plot_zeros=False,
+m.reservoir.output_to_plt(data=property_array, output_props=output_props, lims=lims, plot_zeros=False,
                           aspect_ratio=aspect, logx=logx, cmap=cmap)
 # m.output.output_to_vtk(ith_step=0, output_properties=output_props)  # initial conditions
 plt.savefig('step0.png', format='png')
+plt.show()
 
-for t in range(2):
-    m.run(200)
+data_dt = None
 
-    timesteps, output = m.output.output_properties(output_properties=output_props, timestep=t+1)
+timesteps = [1.e-3, 0.92, 2.-1e-3, 8., 55., 300.] + [365] * 9
+max_ts = [1e-3, 0.01, 0.02, 0.5, 1., 5.] + [20] * 9
 
-    m.reservoir.output_to_plt(data=property_array, output_props=output_props, plot_zeros=False,
+for j, ts in enumerate(timesteps[:2]):
+
+    m.data_ts.dt_mult = 2
+    m.data_ts.dt_max = max_ts[j]
+    m.data_ts.eta[-1] = 100
+
+    if data_dt is not None:
+        data_dt.dt_max = max_ts[j]
+        m.run(data_dt, ts)
+    else:
+        # m.set_sim_params(max_ts=max_ts[j])
+        m.run(ts)
+
+    timesteps, property_array = m.output.output_properties(output_properties=output_props, timestep=-1)
+
+    m.reservoir.output_to_plt(data=property_array, output_props=output_props, lims=lims, plot_zeros=False,
                               aspect_ratio=aspect, logx=logx, cmap=cmap)
 
-    plt.savefig('step' + str(t+1) + '.png', format='png')
+    plt.savefig('step' + str(j+1) + '.png', format='png')
+    plt.show()
 
     # compute and save well time data in m.output_folder
     time_data_dict = m.output.store_well_time_data()
--- models/CCS/model.py
+++ models/CCS/model.py
@@ -12,32 +12,12 @@ from darts.physics.properties.eos_properties import EoSDensity, EoSEnthalpy
 
 
 class Model(DartsModel):
-    def __init__(self):
-        # Call base class constructor
-        super().__init__()
-
-        # Measure time spend on reading/initialization
-        self.timer.node["initialization"].start()
-
-        zero = 1e-10
-        self.set_physics(zero, n_points=1001, temperature=None)
-
-        self.inj_stream = [0.00005]
-        self.t_inj = 350.
-        self.p_inj = 100.
-        self.p_prod = 50.
-
-        self.set_sim_params(first_ts=1e-5, mult_ts=1.5, max_ts=5, tol_newton=1e-3,
-                                    tol_linear=1e-5, it_newton=10, it_linear=50)
-
-        self.timer.node["initialization"].stop()
-
-    def set_reservoir(self, nr, dr, nz, dz, poro, perm):
+    def set_reservoir(self, dr, R1, logspace, nz, dz, poro, perm):
         self.seg_ratio = 1
 
         from darts.reservoirs.struct_radial_reservoir import StructRadialReservoir
-        self.reservoir = StructRadialReservoir(self.timer, nr=nr, nz=nz, dr=dr, dz=dz, permr=perm, permz=perm/10, poro=poro,
-                                               hcap=2200, rcond=100, R1=1000, logspace=True, boundary_volume=4.3e7)
+        self.reservoir = StructRadialReservoir(self.timer, nz=nz, dr=dr, dz=dz, permr=perm, permz=perm/10, poro=poro,
+                                               hcap=2200, rcond=100, R1=R1, nr=40, logspace=logspace, boundary_volume=4.3e7)
         # self.reservoir.well_dict = {'I1': [(1, 1, k+1) for k in range(1, self.reservoir.nz-1)]}
         # self.reservoir.well_dict = {'I1': [(1, 1, k + 1) for k in range(10, 20)]}
         # self.reservoir.well_dict = {'I1': [(1, 1, self.reservoir.nz)]}
@@ -48,28 +28,19 @@ class Model(DartsModel):
 
         return
 
-    def set_wells(self):
-        well_type = ms_well.MS_Type.EPM
-        self.reservoir.add_well("I1", well_type)
-        self.reservoir.add_perforation("I1", res_cell_idx=(1, 1, self.reservoir.nz), well_index=100,
-                                       well_indexD=100)
-
-        self.reservoir.add_well("P1", well_type)
-        for k in range(self.reservoir.nz):
-            self.reservoir.add_perforation("P1", res_cell_idx=(self.reservoir.nx, self.reservoir.ny, k + 1),
-                                           well_index=100, well_indexD=100)
-
-    def set_physics(self,  zero, n_points, temperature=None, temp_inj=350.):
+    def set_physics(self,  zero, n_points, temperature: float = None, ph: bool = False, vl_phases: bool = False):
         """Physical properties"""
         self.zero = zero
 
-        from dartsflash.libflash import CubicEoS, FlashParams, EoS, InitialGuess
+        from dartsflash.libflash import EoS
         from dartsflash.components import CompData
         from dartsflash.mixtures import DARTSFlash, VLAq
         # Fluid components, ions and solid
         components = ["H2O", "CO2"]
-        phases = ["Aq", "V"]
+        self.components = components
+        phases = ["Aq", "V", "L"] if vl_phases else ["Aq", "V"]
         comp_data = CompData(components, setprops=True)
+        nc = len(components)
 
         """ PropertyContainer object and correlations """
         property_container = PropertyContainer(phases_name=phases, components_name=components, Mw=comp_data.Mw,
@@ -77,49 +48,64 @@ class Model(DartsModel):
 
         """ Define flash """
         flash_ev = VLAq(comp_data, hybrid=True)
-
-        flash_ev.set_vl_eos("PR", root_order=[EoS.STABLE])
-        flash_ev.set_aq_eos("Aq", )
+        flash_ev.set_vl_eos("PR", root_order=[EoS.MAX, EoS.MIN] if vl_phases else [EoS.STABLE],
+                            trial_comps=[i for i in range(nc)],
+                            stability_tol=1e-20, switch_tol=1e-2, max_iter=50, use_gmix=False
+                            )
+        flash_ev.set_aq_eos("Aq", stability_tol=1e-20, max_iter=10, use_gmix=True)
         pr = flash_ev.eos["VL"]
         aq = flash_ev.eos["Aq"]
 
-        flash_ev.init_flash(flash_type=DARTSFlash.FlashType.NegativeFlash,
-                            eos_order=["Aq", "VL"], nf_initial_guess=[InitialGuess.Henry_AV])
+        flash_ev.init_flash(flash_type=DARTSFlash.FlashType.PHFlash if ph else DARTSFlash.FlashType.PTFlash,
+                            eos_order=["Aq", "VL"],
+                            t_min=270., t_max=500., t_init=300., t_tol=1e-3,
+                            verbose=True
+                            )
 
         """ properties correlations """
         property_container = PropertyContainer(phases_name=phases, components_name=components, Mw=comp_data.Mw,
                                                temperature=temperature, min_z=zero/10)
 
         property_container.flash_ev = flash_ev
-        property_container.density_ev = dict([('V', EoSDensity(pr, comp_data.Mw)),
-                                              ('Aq', Garcia2001(components))])
+        property_container.density_ev = dict([('V', EoSDensity(eos=flash_ev.eos["VL"], Mw=comp_data.Mw, root_flag=EoS.RootFlag.MAX)),
+                                              ('L', EoSDensity(eos=flash_ev.eos["VL"], Mw=comp_data.Mw, root_flag=EoS.RootFlag.MIN)),
+                                              ('Aq', Garcia2001(components, ions=None, combined_ions=None)), ])
         property_container.viscosity_ev = dict([('V', Fenghour1998()),
-                                                ('Aq', Islam2012(components))])
-        property_container.rel_perm_ev = dict([('V', PhaseRelPerm("gas")),
-                                               ('Aq', PhaseRelPerm("oil"))])
+                                                ('L', Fenghour1998()),
+                                                ('Aq', Islam2012(components, ions=None, combined_ions=None))])
+        diff = 8.64e-6
+        property_container.diffusion_ev = dict([('V', ConstFunc(np.ones(nc) * diff)),
+                                                ('L', ConstFunc(np.ones(nc) * diff)),
+                                                ('Aq', ConstFunc(np.ones(nc) * diff * 1e-3))])
 
-        property_container.enthalpy_ev = dict([('V', EoSEnthalpy(pr)),
-                                               ('Aq', EoSEnthalpy(aq))])
+        property_container.enthalpy_ev = dict([('V', EoSEnthalpy(eos=flash_ev.eos["VL"], root_flag=EoS.RootFlag.MAX)),
+                                               ('L', EoSEnthalpy(eos=flash_ev.eos["VL"], root_flag=EoS.RootFlag.MIN)),
+                                               ('Aq', EoSEnthalpy(eos=flash_ev.eos["Aq"])), ])
+
         property_container.conductivity_ev = dict([('V', ConstFunc(10.)),
+                                                   ('L', ConstFunc(10.)),
                                                    ('Aq', ConstFunc(180.)), ])
 
-        property_container.output_props = {"satA": lambda: property_container.sat[0],
-                                           "satV": lambda: property_container.sat[1],
-                                           "xCO2": lambda: property_container.x[0, 1],
-                                           "yH2O": lambda: property_container.x[1, 0],
-                                           "rhoV": lambda: property_container.dens[1],
+        property_container.rel_perm_ev = dict([('V', PhaseRelPerm("gas", swc=self.swc, sgr=self.swc, n=1.5)),
+                                               ('L', PhaseRelPerm("oil", swc=self.swc, sgr=self.swc, n=1.5)),
+                                               ('Aq', PhaseRelPerm("wat", swc=self.swc, sgr=self.swc, n=4))])
+
+        property_container.output_props = {'temperature': lambda: property_container.temperature,
+                                           'satA': lambda: property_container.sat[phases.index("Aq")],
+                                           'satV': lambda: property_container.sat[phases.index("V")],
+                                           'satL': lambda: property_container.sat[phases.index("L")] if vl_phases else 0.,
+                                           'rho_a': lambda: property_container.dens[phases.index("Aq")],
+                                           'rho_g': lambda: property_container.dens[phases.index("V")],
                                            }
 
         """ Define state specification and initialize Physics object """
         if temperature is None:  # if None, then thermal=True
-            thermal = True
-            state_spec = Compositional.StateSpecification.PT
+            state_spec = Compositional.StateSpecification.PH if ph else Compositional.StateSpecification.PT
         else:
-            thermal = False
             state_spec = Compositional.StateSpecification.P
 
-        self.physics = Compositional(components, phases, self.timer, n_points, min_p=1, max_p=400, min_z=zero/10,
-                                     max_z=1-zero/10, min_t=273.15, max_t=373.15, state_spec=state_spec, cache=False)
+        self.physics = Compositional(components, phases, self.timer, n_points, min_p=1, max_p=500, min_z=zero/10,
+                                     max_z=1-zero/10, min_t=273.15, max_t=473.15, state_spec=state_spec, cache=False)
         self.physics.add_property_region(property_container)
 
         return
@@ -129,12 +115,14 @@ class Model(DartsModel):
             from darts.physics.super.initialize import Initialize
             # depth corresponding to boundary_idx = 10
             b_depth = self.reservoir.global_data['depth'].min() + (self.reservoir.global_data['depth'].max() - self.reservoir.global_data['depth'].min()) / 4.
-            boundary_state = {'H2O': 1 - self.zero, 'pressure': 100., 'temperature': 350.}
+            boundary_state = {'H2O': 0.92, 'pressure': self.p_init, 'temperature': self.t_init}
             init = Initialize(physics=self.physics)
+            primary_specs = {} #{'H2O': 1 - self.zero}
+            secondary_specs = {'satAq': self.swc}
             X = init.solve(depth_bottom=self.reservoir.global_data['depth'].max(),
                            depth_top=self.reservoir.global_data['depth'].min(),
                            depth_known=b_depth, boundary_state=boundary_state,
-                           primary_specs={'H2O': 1 - self.zero}, secondary_specs={})
+                           primary_specs=primary_specs, secondary_specs=secondary_specs)
             self.physics.set_initial_conditions_from_depth_table(mesh=self.reservoir.mesh, input_depth=init.depths,
                                                                  input_distribution={var: X[i::self.physics.n_vars] for i, var in
                                                                                      enumerate(self.physics.vars)})
@@ -146,6 +134,19 @@ class Model(DartsModel):
             return self.physics.set_initial_conditions_from_array(mesh=self.reservoir.mesh,
                                                                   input_distribution=input_distribution)
 
+    def set_wells(self):
+        if 1:
+            self.reservoir.add_well("I1")
+            for k in range(4, 20):
+            # for k in range(24):
+                self.reservoir.add_perforation("I1", res_cell_idx=(1, 1, k + 1), well_indexD=0)
+                # self.reservoir.add_perforation("I1", res_cell_idx=(1, 1, k + 1), well_indexD=0, ms_epm=True)
+
+        if 0:
+            self.reservoir.add_well("P1", well_type)
+            for k in range(4, 20):
+                self.reservoir.add_perforation("P1", res_cell_idx=(1, 1, k + 1), well_index=100, well_indexD=100)
+
     def set_well_controls(self):
         from darts.engines import well_control_iface
         for i, w in enumerate(self.reservoir.wells):
@@ -153,6 +154,11 @@ class Model(DartsModel):
                 self.physics.set_well_controls(wctrl=w.control, control_type=well_control_iface.BHP,
                                                is_inj=True, target=self.p_inj, inj_composition=self.inj_stream,
                                                inj_temp=self.t_inj)
+                # self.physics.set_well_controls(wctrl=w.control, is_inj=True,
+                #                                control_type=well_control_iface.BHP, target=self.p_inj,
+                #                                # control_type=well_control_iface.MOLAR_RATE, target=4000.,
+                #                                inj_composition=self.inj_stream,
+                #                                inj_temp=self.t_inj)
             else:
                 self.physics.set_well_controls(wctrl=w.control, control_type=well_control_iface.BHP,
                                                is_inj=False, target=self.p_prod)
