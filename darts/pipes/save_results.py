a619d7665d46c73506980977f57bf83fb8875959
smoslehi <s.moslehi@tudelft.nl>
2026-02-08 02:09:05 +0100

    Shorten property_container to pc in save_results.py

--- darts/pipes/save_results.py
+++ darts/pipes/save_results.py
@@ -13,39 +13,39 @@ def save_segments_primary_vars_and_phase_props(h5_well_data, coupled_model):
     :param coupled_model: The instance of the DartsModel
     :type coupled_model: DartsModel
     """
-    property_container = coupled_model.physics.property_containers[0]
+    pc = coupled_model.physics.property_containers[0]
 
     num_perfs = len(coupled_model.reservoir.wells[0].perforations)
     num_segments = len(h5_well_data["dynamic"]["X"][0, :, 0]) - num_perfs
 
     p = np.zeros(num_segments)
-    z = np.zeros((num_segments, property_container.nc))
+    z = np.zeros((num_segments, pc.nc))
     T = np.zeros(num_segments)
 
     sG = np.zeros(num_segments)
-    if property_container.nph == 2:
+    if pc.nph == 2:
         sL = np.zeros(num_segments)
-    elif property_container.nph == 3:
+    elif pc.nph == 3:
         sL_a = np.zeros(num_segments)
         sL_b = np.zeros(num_segments)
     rhoG = np.zeros(num_segments)
-    if property_container.nph == 2:
+    if pc.nph == 2:
         rhoL = np.zeros(num_segments)
-    elif property_container.nph == 3:
+    elif pc.nph == 3:
         rhoL_a = np.zeros(num_segments)
         rhoL_b = np.zeros(num_segments)
     miuG = np.zeros(num_segments)
-    if property_container.nph == 2:
+    if pc.nph == 2:
         miuL = np.zeros(num_segments)
-    elif property_container.nph == 3:
+    elif pc.nph == 3:
         miuL_a = np.zeros(num_segments)
         miuL_b = np.zeros(num_segments)
-    xG = np.zeros((num_segments, property_container.nc))
-    if property_container.nph == 2:
-        xL = np.zeros((num_segments, property_container.nc))
-    elif property_container.nph == 3:
-        xL_a = np.zeros((num_segments, property_container.nc))
-        xL_b = np.zeros((num_segments, property_container.nc))
+    xG = np.zeros((num_segments, pc.nc))
+    if pc.nph == 2:
+        xL = np.zeros((num_segments, pc.nc))
+    elif pc.nph == 3:
+        xL_a = np.zeros((num_segments, pc.nc))
+        xL_b = np.zeros((num_segments, pc.nc))
     # Initialize an empty DataFrame to store the primary variables and phase props
     data_frame = pd.DataFrame()
 
@@ -63,13 +63,13 @@ def save_segments_primary_vars_and_phase_props(h5_well_data, coupled_model):
             p[j] = state[0]
 
             # Evaluate temperature for when the primary vars are PH and evaluate phase props
-            property_container.evaluate(state)
+            pc.evaluate(state)
 
-            if not coupled_model.physics.property_containers[0].thermal:
+            if not pc.thermal:
                 z_full = state[1:]
                 z_full = np.append(z_full, 1 - sum(state[1:]))
                 z[j, :] = z_full
-            elif coupled_model.physics.property_containers[0].thermal:
+            elif pc.thermal:
                 z_full = state[1:-1]
                 z_full = np.append(z_full, 1 - sum(state[1:-1]))
                 z[j, :] = z_full
@@ -82,40 +82,40 @@ def save_segments_primary_vars_and_phase_props(h5_well_data, coupled_model):
                     coupled_model.physics.state_spec
                     == coupled_model.physics.StateSpecification.PH
                 ):
-                    T[j] = property_container.temperature
+                    T[j] = pc.temperature
 
-            # xG[j,:] = property_container.x[1,:]
-            xG[j, :] = property_container.x[0, :]
-            if property_container.nph == 2:
-                xL[j, :] = property_container.x[1, :]
-            elif property_container.nph == 3:
-                # xL_a[j, :] = property_container.x[0, :]
-                xL_a[j, :] = property_container.x[1, :]
-                xL_b[j, :] = property_container.x[2, :]
-            # sG[j] = property_container.sat[1]
-            sG[j] = property_container.sat[0]
-            if property_container.nph == 2:
-                sL[j] = property_container.sat[1]
-            elif property_container.nph == 3:
-                # sL_a[j] = property_container.sat[0]
-                sL_a[j] = property_container.sat[1]
-                sL_b[j] = property_container.sat[2]
-            # rhoG[j] = property_container.dens[1]
-            rhoG[j] = property_container.dens[0]
-            if property_container.nph == 2:
-                rhoL[j] = property_container.dens[1]
-            elif property_container.nph == 3:
-                # rhoL_a[j] = property_container.dens[0]
-                rhoL_a[j] = property_container.dens[1]
-                rhoL_b[j] = property_container.dens[2]
-            # miuG[j] = property_container.mu[1]
-            miuG[j] = property_container.mu[0]
-            if property_container.nph == 2:
-                miuL[j] = property_container.mu[1]
-            elif property_container.nph == 3:
-                # miuL_a[j] = property_container.mu[0]
-                miuL_a[j] = property_container.mu[1]
-                miuL_b[j] = property_container.mu[2]
+            # xG[j,:] = pc.x[1,:]
+            xG[j, :] = pc.x[0, :]
+            if pc.nph == 2:
+                xL[j, :] = pc.x[1, :]
+            elif pc.nph == 3:
+                # xL_a[j, :] = pc.x[0, :]
+                xL_a[j, :] = pc.x[1, :]
+                xL_b[j, :] = pc.x[2, :]
+            # sG[j] = pc.sat[1]
+            sG[j] = pc.sat[0]
+            if pc.nph == 2:
+                sL[j] = pc.sat[1]
+            elif pc.nph == 3:
+                # sL_a[j] = pc.sat[0]
+                sL_a[j] = pc.sat[1]
+                sL_b[j] = pc.sat[2]
+            # rhoG[j] = pc.dens[1]
+            rhoG[j] = pc.dens[0]
+            if pc.nph == 2:
+                rhoL[j] = pc.dens[1]
+            elif pc.nph == 3:
+                # rhoL_a[j] = pc.dens[0]
+                rhoL_a[j] = pc.dens[1]
+                rhoL_b[j] = pc.dens[2]
+            # miuG[j] = pc.mu[1]
+            miuG[j] = pc.mu[0]
+            if pc.nph == 2:
+                miuL[j] = pc.mu[1]
+            elif pc.nph == 3:
+                # miuL_a[j] = pc.mu[0]
+                miuL_a[j] = pc.mu[1]
+                miuL_b[j] = pc.mu[2]
 
         # Save phase velocities
         if i == 0:
@@ -138,7 +138,7 @@ def save_segments_primary_vars_and_phase_props(h5_well_data, coupled_model):
         vG = np.append(vG, np.nan)
         vL = np.append(vL, np.nan)
 
-        if property_container.nph == 2:
+        if pc.nph == 2:
             ts_primary_vars_and_phases_props = [
                 p.copy(),
                 z.copy(),
@@ -177,7 +177,7 @@ def save_segments_primary_vars_and_phase_props(h5_well_data, coupled_model):
                     ),
                 ]
             )
-        elif property_container.nph == 3:
+        elif pc.nph == 3:
             ts_primary_vars_and_phases_props = [
                 p.copy(),
                 z.copy(),
