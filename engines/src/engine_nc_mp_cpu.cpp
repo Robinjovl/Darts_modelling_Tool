36f24e7b53a44ac577c0690389376305b4510135
smoslehi <s.moslehi@tudelft.nl>
2026-03-04 16:03:54 +0100

    Update GPU source files

--- engines/src/engine_nc_mp_cpu.cpp
+++ engines/src/engine_nc_mp_cpu.cpp
@@ -22,17 +22,19 @@
 template <uint8_t NC>
 int engine_nc_mp_cpu<NC>::init(conn_mesh *mesh_, std::vector<ms_well *> &well_list_,
 							   std::vector<operator_set_gradient_evaluator_iface *> &acc_flux_op_set_list_,
+							   operator_set_gradient_evaluator_iface* thermal_var_etor_,
 							   sim_params *params_, timer_node *timer_)
 {
 	TWO_POINT_RES_ASSEMBLY = false;
 	USE_CALCULATED_FLUX = false;
-	init_base(mesh_, well_list_, acc_flux_op_set_list_, params_, timer_);
+	init_base(mesh_, well_list_, acc_flux_op_set_list_, thermal_var_etor_, params_, timer_);
 	return 0;
 }
 
 template <uint8_t NC>
 int engine_nc_mp_cpu<NC>::init_base(conn_mesh *mesh_, std::vector<ms_well *> &well_list_,
 									std::vector<operator_set_gradient_evaluator_iface *> &acc_flux_op_set_list_,
+									operator_set_gradient_evaluator_iface* thermal_var_etor_,
 									sim_params *params_, timer_node *timer_)
 {
 	time_t rawtime;
@@ -231,16 +233,16 @@ int engine_nc_mp_cpu<NC>::init_base(conn_mesh *mesh_, std::vector<ms_well *> &we
 	n_vars = get_n_vars();
 	n_ops = get_n_ops();
 	nc = get_n_comps();
-	z_var = get_z_var();
+	z_var_idx = get_z_var_idx();
 	if (params->log_transform == 0)
 	{
-		min_axis_z = acc_flux_op_set_list[0]->get_axis_min(z_var);
-		max_axis_z = acc_flux_op_set_list[0]->get_axis_max(z_var);
+		min_axis_z = acc_flux_op_set_list[0]->get_axis_min(z_var_idx);
+		max_axis_z = acc_flux_op_set_list[0]->get_axis_max(z_var_idx);
 	}
 	else if (params->log_transform == 1)
 	{
-		min_axis_z = std::exp(acc_flux_op_set_list[0]->get_axis_min(z_var));
-		max_axis_z = std::exp(acc_flux_op_set_list[0]->get_axis_max(z_var));
+		min_axis_z = std::exp(acc_flux_op_set_list[0]->get_axis_min(z_var_idx));
+		max_axis_z = std::exp(acc_flux_op_set_list[0]->get_axis_max(z_var_idx));
 	}
 	min_sim_z = min_axis_z + params->sim_eps;
 	max_sim_z = max_axis_z - params->sim_eps;
