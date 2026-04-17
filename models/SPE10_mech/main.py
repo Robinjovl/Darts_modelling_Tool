f5d168f3067d2fbdd082a5df75ad266a2a2e0c42
Ilshat Saifullin <I.S.Saifullin@tudelft.nl>
2026-04-18 01:30:11 +0200

    short run

--- models/SPE10_mech/main.py
+++ models/SPE10_mech/main.py
@@ -283,8 +283,8 @@ if __name__ == '__main__':
     report_step = 365.25 / 4
 
     # short run
-    #sim_time = 90 # days
-    #report_step = sim_time  # days
+    sim_time = 30 # days
+    report_step = sim_time  # days
 
     run(model_folder=mesh, physics_type=physics_type, generate_mesh=generate_mesh, wells_type=wells_type, decouple_geomech=decouple_geomech, report_step=report_step, sim_time=sim_time)
 
