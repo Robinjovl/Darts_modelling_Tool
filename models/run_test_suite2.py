ae95a235c67df6fde8aba942c76b92ff3dcad419
Ilshat Saifullin <I.S.Saifullin@tudelft.nl>
2026-09-03 11:47:27 +0200

    run the THM_vs_proxy model only for iter_solvers

--- models/run_test_suite2.py
+++ models/run_test_suite2.py
@@ -110,15 +110,14 @@ def run_testing(platform, overwrite, iter_solvers, test_all_models):
     test_dirs_mech += ['1ph_1comp_poroelastic_convergence']
     test_args_mech = [test_args_mech, [['']]]  # no args for the convergence test
 
-    # the cases are compared with vtk reference solutions too
-    test_dirs_mech += ['THM_vs_geomech_proxy']
-    test_args_mech += [[
-        ['case_1',   'single_phase_thermal'], # check whether it runs only: its reference is too large (see CASES_WITHOUT_REF there)
-        ['17_17_15', 'single_phase'],
-        ['17_17_15', 'single_phase_thermal'],
-    ]]
-
     if iter_solvers:
+        test_dirs_mech += ['THM_vs_geomech_proxy']
+        test_args_mech += [[
+            ['case_1',   'single_phase_thermal'], # case_1 is checked whether it runs only: its reference is too large (see CASES_WITHOUT_REF there)
+            ['17_17_15', 'single_phase'],
+            ['17_17_15', 'single_phase_thermal'],
+        ]]
+    
         test_dirs_mech += ['SPE10_mech']
         physics_list = ['single_phase', 'single_phase_thermal', 'dead_oil', 'dead_oil_thermal']
         meshes_list = ['data_10_10_10']
